630c8cfffbe77d0db0dad03255b9c60c4433e8f6
Daniel <danielBeets@protonmail.com>
2021-11-22 22:04:58 -1000

    Proposal for a simplified join/exit interface

---
+++ balancer-js/src/pool-weighted/interface.ts
@@ -0,0 +1,66 @@
+import { BigNumberish } from '@ethersproject/bignumber';
+
+export interface PoolService {
+    /**
+     * Allow the input of any number of pool tokens, return the expected
+     * bptAmount and priceImpactPercent of the join.
+     *
+     * Internally do sanity checks to ensure that all input tokens are actually part of the pool.
+     */
+    queryJoin(poolId: string, tokens: SimpleToken[]): Promise<QueryJoinOutput>;
+    /**
+     * Given a single token as input, determine what a zero impact join looks like.
+     * Also return the expected bptAmount for a zero impact join.
+     */
+    queryBalancedJoin(poolId: string, token: SimpleToken): Promise<QueryBalancedJoinOutput>;
+    /**
+     * Given the input tokens, bptAmount, and maxSlippagePercent, encode the join operation.
+     */
+    encodeJoin(input: EncodeJoinInput): Promise<void>;
+
+    //TODO: having the queries resolve to a promise may not be performant enough (ie: the frontend percentage slider)
+    //TODO: Alternatively, we could pass the Pool in already resolved, or somehow cache the Pool in the SDK for some period of time.
+    queryBalancedExit(poolId: string, bptAmount: BigNumberish): Promise<QueryBalancedExitOutput>;
+    querySingleTokenExit(
+        poolId: string,
+        bptAmount: BigNumberish,
+        tokenAddress: string,
+    ): Promise<QuerySingleTokenExitOutput>;
+
+    encodeBalancedExit(poolId: string, bptAmount: BigNumberish, maxSlippagePercent: number): Promise<void>;
+    encodeSingleTokenExit(
+        poolId: string,
+        bptAmount: BigNumberish,
+        tokenAddress: string,
+        maxSlippagePercent: number,
+    ): Promise<void>;
+}
+
+interface QueryJoinOutput {
+    bptAmount: BigNumberish;
+    priceImpactPercent: number;
+}
+
+interface QueryBalancedJoinOutput {
+    tokens: SimpleToken[];
+    bptAmount: BigNumberish;
+}
+
+interface SimpleToken {
+    address: string;
+    amount: BigNumberish;
+}
+
+interface EncodeJoinInput {
+    tokens: SimpleToken[];
+    bptAmount: BigNumberish;
+    maxSlippagePercent: number;
+}
+
+interface QueryBalancedExitOutput {
+    tokens: SimpleToken[];
+}
+
+interface QuerySingleTokenExitOutput {
+    token: SimpleToken;
+}
