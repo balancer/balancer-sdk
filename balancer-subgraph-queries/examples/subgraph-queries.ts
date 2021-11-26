68d89b4f586a8c882f9388c11223e92f84e8a03a
Daniel <danielBeets@protonmail.com>
2021-11-25 23:57:05 -0800

    add a dev .env and a .gitignore, clean up examples

--- balancer-subgraph-queries/examples/subgraph-queries.ts
+++ balancer-subgraph-queries/examples/subgraph-queries.ts
@@ -1,19 +1,24 @@
-import dotenv from 'dotenv';
-import { createBalancerSubgraphClient } from '../src/client';
-import { OrderDirection, User_OrderBy } from '../generated/balancer-subgraph-types';
+import * as dotenv from "dotenv";
+import { createBalancerSubgraphClient } from "../src/client";
+import {
+  OrderDirection,
+  User_OrderBy,
+} from "../generated/balancer-subgraph-types";
 
 dotenv.config();
 
 async function subgraphQueries() {
-    const client = createBalancerSubgraphClient(process.env.BALANCER_SUBGRAPH);
+  const client = createBalancerSubgraphClient(process.env.BALANCER_SUBGRAPH);
 
-    const { pools } = await client.BalancerPools({ first: 5 });
-    const { pools: filteredPools } = await client.BalancerPools({ where: { totalLiquidity_gt: '1' } });
+  const { pools } = await client.BalancerPools({ first: 5 });
+  const { pools: filteredPools } = await client.BalancerPools({
+    where: { totalLiquidity_gt: "1" },
+  });
 
-    const { user } = await client.BalancerUser({ id: 'user-address' });
-    const { users } = await client.BalancerUsers({
-        first: 5,
-        orderBy: User_OrderBy.SharesOwned,
-        orderDirection: OrderDirection.Desc,
-    });
+  const { user } = await client.BalancerUser({ id: "user-address" });
+  const { users } = await client.BalancerUsers({
+    first: 5,
+    orderBy: User_OrderBy.SharesOwned,
+    orderDirection: OrderDirection.Desc,
+  });
 }
