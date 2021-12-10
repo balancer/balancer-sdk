74997fc6e563a7ac2bbb9a7fe61b940dfb7438e3
johngrantuk <johngrantuk@googlemail.com>
2021-12-10 09:39:33 +0000

    Updated for new linear pools and test deployment addresses.

--- balancer-js/examples/constants.ts
+++ balancer-js/examples/constants.ts
@@ -5,7 +5,7 @@ interface TestToken {
 
 // Kovan version
 export const STABAL3PHANTOM: TestToken = {
-    address: '0x21ff756ca0cfcc5fff488ad67babadffee0c4149',
+    address: '0x8fd162f338b770f7e879030830cde9173367f301',
     decimals: 18,
 };
 
--- balancer-js/examples/relayerSwapUnwrap.ts
+++ balancer-js/examples/relayerSwapUnwrap.ts
@@ -5,9 +5,20 @@ import { Wallet } from '@ethersproject/wallet';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Contract } from '@ethersproject/contracts';
 
-import { BalancerSDK, Network, ConfigSdk, SUBGRAPH_URLS, AaveHelpers } from '../src/index';
+import {
+    BalancerSDK,
+    Network,
+    ConfigSdk,
+    SUBGRAPH_URLS,
+    AaveHelpers,
+} from '../src/index';
 import { FundManagement } from '../src/swapsService/types';
-import { WRAPPED_AAVE_DAI, WRAPPED_AAVE_USDC, WRAPPED_AAVE_USDT, STABAL3PHANTOM } from './constants';
+import {
+    WRAPPED_AAVE_DAI,
+    WRAPPED_AAVE_USDC,
+    WRAPPED_AAVE_USDT,
+    STABAL3PHANTOM,
+} from './constants';
 
 import balancerRelayerAbi from '../src/abi/BalancerRelayer.json';
 
@@ -23,8 +34,8 @@ async function runRelayerSwapUnwrapExactIn() {
     const config: ConfigSdk = {
         network: Network.KOVAN,
         rpcUrl: `https://kovan.infura.io/v3/${process.env.INFURA}`,
-        subgraphUrl: SUBGRAPH_URLS[Network.KOVAN]
-    } 
+        subgraphUrl: SUBGRAPH_URLS[Network.KOVAN],
+    };
 
     const provider = new JsonRpcProvider(config.rpcUrl);
     const key: any = process.env.TRADER_KEY;
@@ -42,20 +53,45 @@ async function runRelayerSwapUnwrapExactIn() {
     };
 
     // This is using a helper function to get the up to date rates for the Aave tokens
-    const daiRate = await AaveHelpers.getRate('0x26575a44755e0aaa969fdda1e4291df22c5624ea', provider);
-    const usdcRate = await AaveHelpers.getRate('0x26743984e3357eFC59f2fd6C1aFDC310335a61c9', provider);
-    const usdtRate = await AaveHelpers.getRate('0xbfd9769b061e57e478690299011a028194d66e3c', provider);
+    const daiRate = await AaveHelpers.getRate(
+        '0x26575a44755e0aaa969fdda1e4291df22c5624ea',
+        provider
+    );
+    const usdcRate = await AaveHelpers.getRate(
+        '0x26743984e3357eFC59f2fd6C1aFDC310335a61c9',
+        provider
+    );
+    const usdtRate = await AaveHelpers.getRate(
+        '0xbfd9769b061e57e478690299011a028194d66e3c',
+        provider
+    );
 
     const txInfo = await balancer.relayer.swapUnwrapAaveStaticExactIn(
-        [STABAL3PHANTOM.address, STABAL3PHANTOM.address, STABAL3PHANTOM.address],
-        [WRAPPED_AAVE_DAI.address, WRAPPED_AAVE_USDC.address, WRAPPED_AAVE_USDT.address],
-        [parseFixed('1', 16).toString(), parseFixed('1', 16).toString(), parseFixed('1', 16).toString()],
+        [
+            STABAL3PHANTOM.address,
+            STABAL3PHANTOM.address,
+            STABAL3PHANTOM.address,
+        ],
+        [
+            WRAPPED_AAVE_DAI.address,
+            WRAPPED_AAVE_USDC.address,
+            WRAPPED_AAVE_USDT.address,
+        ],
+        [
+            parseFixed('1', 16).toString(),
+            parseFixed('1', 16).toString(),
+            parseFixed('1', 16).toString(),
+        ],
         [daiRate, usdcRate, usdtRate],
         funds,
         '50000000000000000' // Slippage 5%
     );
 
-    const relayerContract = new Contract(relayerAddress, balancerRelayerAbi, provider);
+    const relayerContract = new Contract(
+        relayerAddress,
+        balancerRelayerAbi,
+        provider
+    );
     const tx = await relayerContract
         .connect(wallet)
         .callStatic[txInfo.function](txInfo.params, {
@@ -79,7 +115,7 @@ async function runRelayerSwapUnwrapExactOut() {
     const config: ConfigSdk = {
         network: Network.KOVAN,
         rpcUrl: `https://kovan.infura.io/v3/${process.env.INFURA}`,
-        subgraphUrl: SUBGRAPH_URLS[Network.KOVAN]
+        subgraphUrl: SUBGRAPH_URLS[Network.KOVAN],
     };
 
     const provider = new JsonRpcProvider(config.rpcUrl);
@@ -98,20 +134,41 @@ async function runRelayerSwapUnwrapExactOut() {
     };
 
     // This is using a helper function to get the up to date rates for the Aave tokens
-    const daiRate = await AaveHelpers.getRate('0x26575a44755e0aaa969fdda1e4291df22c5624ea', provider);
-    const usdcRate = await AaveHelpers.getRate('0x26743984e3357eFC59f2fd6C1aFDC310335a61c9', provider);
-    const usdtRate = await AaveHelpers.getRate('0xbfd9769b061e57e478690299011a028194d66e3c', provider);
+    const daiRate = await AaveHelpers.getRate(
+        '0x26575a44755e0aaa969fdda1e4291df22c5624ea',
+        provider
+    );
+    const usdcRate = await AaveHelpers.getRate(
+        '0x26743984e3357eFC59f2fd6C1aFDC310335a61c9',
+        provider
+    );
+    const usdtRate = await AaveHelpers.getRate(
+        '0xbfd9769b061e57e478690299011a028194d66e3c',
+        provider
+    );
 
     const txInfo = await balancer.relayer.swapUnwrapAaveStaticExactOut(
-        [STABAL3PHANTOM.address, STABAL3PHANTOM.address, STABAL3PHANTOM.address],
-        [WRAPPED_AAVE_DAI.address, WRAPPED_AAVE_USDC.address, WRAPPED_AAVE_USDT.address],
-        [parseFixed('1', 16).toString(), '1000', '1000'],  // Amount of unwrapped Aave token we want to receive
+        [
+            STABAL3PHANTOM.address,
+            STABAL3PHANTOM.address,
+            STABAL3PHANTOM.address,
+        ],
+        [
+            WRAPPED_AAVE_DAI.address,
+            WRAPPED_AAVE_USDC.address,
+            WRAPPED_AAVE_USDT.address,
+        ],
+        [parseFixed('1', 16).toString(), '1000', '1000'], // Amount of unwrapped Aave token we want to receive
         [daiRate, usdcRate, usdtRate],
         funds,
         '50000000000000000' // Slippage 5%
     );
 
-    const relayerContract = new Contract(relayerAddress, balancerRelayerAbi, provider);
+    const relayerContract = new Contract(
+        relayerAddress,
+        balancerRelayerAbi,
+        provider
+    );
     const tx = await relayerContract
         .connect(wallet)
         .callStatic[txInfo.function](txInfo.params, {
