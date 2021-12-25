3e5b7f23c2d0390cb8c8b6d691dfaf5d05281c84
Daniel <danielBeets@protonmail.com>
2021-12-25 10:27:22 +0100

    Change prefix to Subgraph from Balancer

--- balancer-js/src/subgraph/examples/subgraph-queries.ts
+++ balancer-js/src/subgraph/examples/subgraph-queries.ts
@@ -10,13 +10,13 @@ dotenv.config();
 async function subgraphQueries() {
     const client = createSubgraphClient(process.env.BALANCER_SUBGRAPH_URL!);
 
-    const { pools } = await client.BalancerPools({ first: 5 });
-    const { pools: filteredPools } = await client.BalancerPools({
+    const { pools } = await client.SubgraphPools({ first: 5 });
+    const { pools: filteredPools } = await client.SubgraphPools({
         where: { totalLiquidity_gt: '1' },
     });
 
-    const { user } = await client.BalancerUser({ id: 'user-address' });
-    const { users } = await client.BalancerUsers({
+    const { user } = await client.SubgraphUser({ id: 'user-address' });
+    const { users } = await client.SubgraphUsers({
         first: 5,
         orderBy: User_OrderBy.SharesOwned,
         orderDirection: OrderDirection.Desc,
