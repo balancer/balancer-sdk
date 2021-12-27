c3fc2641140fbdd25c75dfc4958cafbe9dbb6e0c
Daniel <danielBeets@protonmail.com>
2021-12-27 11:54:36 +0100

    Create a SORFactory
    - Migrate coingecko token price fetching
    - Implement a subgraph token price service
    - Migrate subgraph pool fetching
    - Migrate on chain balance fetching.

---
+++ balancer-js/src/sor/sorFactory.ts
@@ -0,0 +1,61 @@
+import { SOR, TokenPriceService } from '@balancer-labs/sor';
+import { Provider } from '@ethersproject/providers';
+import { SubgraphPoolDataService } from './pool-data/subgraphPoolDataService';
+import { CoingeckoTokenPriceService } from './token-price/coingeckoTokenPriceService';
+import { SubgraphClient } from '../subgraph/subgraph';
+import { BalancerNetworkConfig, BalancerSdkSorConfig } from '../types';
+import { SubgraphTokenPriceService } from './token-price/subgraphTokenPriceService';
+
+export class SorFactory {
+    public static createSor(
+        network: BalancerNetworkConfig,
+        sorConfig: BalancerSdkSorConfig,
+        provider: Provider,
+        subgraphClient: SubgraphClient
+    ): SOR {
+        const poolDataService = SorFactory.getPoolDataService(
+            network,
+            sorConfig,
+            provider,
+            subgraphClient
+        );
+
+        const tokenPriceService = SorFactory.getTokenPriceService(
+            network,
+            sorConfig,
+            subgraphClient
+        );
+
+        return new SOR(provider, network, poolDataService, tokenPriceService);
+    }
+
+    private static getPoolDataService(
+        network: BalancerNetworkConfig,
+        sorConfig: BalancerSdkSorConfig,
+        provider: Provider,
+        subgraphClient: SubgraphClient
+    ) {
+        return typeof sorConfig.poolDataService === 'object'
+            ? sorConfig.poolDataService
+            : new SubgraphPoolDataService(
+                  subgraphClient,
+                  provider,
+                  network,
+                  sorConfig
+              );
+    }
+
+    private static getTokenPriceService(
+        network: BalancerNetworkConfig,
+        sorConfig: BalancerSdkSorConfig,
+        subgraphClient: SubgraphClient
+    ): TokenPriceService {
+        if (typeof sorConfig.tokenPriceService === 'object') {
+            return sorConfig.tokenPriceService;
+        } else if (sorConfig.tokenPriceService === 'subgraph') {
+            new SubgraphTokenPriceService(subgraphClient, network.weth);
+        }
+
+        return new CoingeckoTokenPriceService(network.chainId);
+    }
+}
