66432a272e86da9eace172586848c428605246d0
Gareth Fuller <gareth.a.f@gmail.com>
2022-01-31 16:42:59 +0000

    Make subgraph folder a module

---
+++ balancer-js/src/modules/subgraph/subgraph.module.ts
@@ -0,0 +1,20 @@
+import { BalancerSdkConfig } from '@/types';
+import { GraphQLClient } from 'graphql-request';
+import { BalancerSDK } from '../sdk.module';
+import { getSdk } from './generated/balancer-subgraph-types';
+import { SubgraphClient } from './subgraph';
+
+export class Subgraph {
+    public url: string;
+    public client: SubgraphClient;
+
+    constructor(config: BalancerSdkConfig) {
+        this.url = BalancerSDK.getNetworkConfig(config).subgraphUrl;
+        this.client = this.initClient();
+    }
+
+    private initClient(): SubgraphClient {
+        const client = new GraphQLClient(this.url);
+        return getSdk(client);
+    }
+}
--- balancer-js/src/sor/sorFactory.ts
+++ balancer-js/src/sor/sorFactory.ts
@@ -1,19 +1,22 @@
 import { SOR, TokenPriceService } from '@balancer-labs/sor';
 import { Provider, JsonRpcProvider } from '@ethersproject/providers';
-import { BALANCER_NETWORK_CONFIG } from '@/lib/constants/contracts';
 import { SubgraphPoolDataService } from './pool-data/subgraphPoolDataService';
 import { CoingeckoTokenPriceService } from './token-price/coingeckoTokenPriceService';
-import { SubgraphClient, createSubgraphClient } from '../subgraph/subgraph';
+import {
+    SubgraphClient,
+    createSubgraphClient,
+} from '@/modules/subgraph/subgraph';
 import {
     BalancerNetworkConfig,
     BalancerSdkConfig,
     BalancerSdkSorConfig,
 } from '../types';
 import { SubgraphTokenPriceService } from './token-price/subgraphTokenPriceService';
+import { BalancerSDK } from '..';
 
 export class SorFactory {
     public static createSor(sdkConfig: BalancerSdkConfig): SOR {
-        const network = this.getNetworkConfig(sdkConfig);
+        const network = BalancerSDK.getNetworkConfig(sdkConfig);
         const sorConfig = SorFactory.getSorConfig(sdkConfig);
         const provider = new JsonRpcProvider(sdkConfig.rpcUrl);
         const subgraphClient = createSubgraphClient(network.subgraphUrl);
@@ -32,25 +35,6 @@ export class SorFactory {
         );
 
         return new SOR(provider, network, poolDataService, tokenPriceService);
-    }
-
-    private static getNetworkConfig(
-        config: BalancerSdkConfig
-    ): BalancerNetworkConfig {
-        if (typeof config.network === 'number') {
-            const networkConfig = BALANCER_NETWORK_CONFIG[config.network];
-
-            return {
-                ...networkConfig,
-                subgraphUrl:
-                    config.customSubgraphUrl ?? networkConfig.subgraphUrl,
-            };
-        }
-
-        return {
-            ...config.network,
-            subgraphUrl: config.customSubgraphUrl ?? config.network.subgraphUrl,
-        };
     }
 
     private static getSorConfig(
