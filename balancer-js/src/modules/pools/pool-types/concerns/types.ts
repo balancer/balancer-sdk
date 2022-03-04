8a98139654957788575d331b33e90bc28969c156
Gareth Fuller <gareth.a.f@gmail.com>
2022-03-04 21:00:51 +0000

    Remove input types for now

--- balancer-js/src/modules/pools/pool-types/concerns/types.ts
+++ balancer-js/src/modules/pools/pool-types/concerns/types.ts
@@ -1,10 +1,3 @@
-import { BigNumberish } from '@ethersproject/bignumber';
-
 export interface LiquidityConcern {
-    calcTotal: (
-        tokenBalances: BigNumberish[],
-        tokenDecimals: number[],
-        tokenPriceRates: BigNumberish[],
-        tokenPrices: (number | null)[]
-    ) => string;
+    calcTotal: () => string;
 }
