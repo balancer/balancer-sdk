8d398f42590398203f68a22d325fa8ee18d80ec7
Tim Robinson <tim@timjrobinson.com>
2022-05-12 00:01:36 +1000

    Working on calculate liquidity

--- balancer-js/src/modules/pools/pool-types/concerns/weighted/liquidity.concern.ts
+++ balancer-js/src/modules/pools/pool-types/concerns/weighted/liquidity.concern.ts
@@ -1,21 +1,20 @@
 import { LiquidityConcern } from '../types';
 import { BigNumberish } from '@ethersproject/bignumber';
 
+export interface TokenBalance {
+    balance: BigNumberish;
+    decimals: number;
+    priceRate: BigNumberish;
+    price: BigNumberish;
+}
+
 export class WeightedPoolLiquidity implements LiquidityConcern {
     calcTotal(
-        tokenBalances: BigNumberish[],
-        tokenDecimals: number[],
-        tokenPriceRates: BigNumberish[],
-        tokenPrices: (number | null)[],
-        someOtherInput: number
+        tokenBalances: TokenBalance[],
     ): string {
         // TODO implementation
         console.log(
             tokenBalances,
-            tokenDecimals,
-            tokenPriceRates,
-            tokenPrices,
-            someOtherInput
         );
         throw new Error('To be implemented');
         return '100';
