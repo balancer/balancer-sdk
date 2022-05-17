b2cc747ff5c13e0e9b36535a3ac355a27fa3e9d5
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-05-17 19:29:27 -0300

    Create exactTokensJoinPool function
    Add exactTokensJoinPool for Weighted Pools to pools module.

--- balancer-js/src/modules/pools/pools.module.ts
+++ balancer-js/src/modules/pools/pools.module.ts
@@ -1,4 +1,12 @@
-import { BalancerSdkConfig } from '@/types';
+import { BigNumber } from '@ethersproject/bignumber';
+import { WeiPerEther } from '@ethersproject/constants';
+
+import { BalancerSdkConfig, JoinPoolRequest, TransactionData } from '@/types';
+import {
+    EncodeJoinPoolInput,
+    JoinPoolData,
+    ExactTokensJoinPoolInput,
+} from './types';
 import { Stable } from './pool-types/stable.module';
 import { Weighted } from './pool-types/weighted.module';
 import { MetaStable } from './pool-types/metaStable.module';
@@ -6,6 +14,10 @@ import { StablePhantom } from './pool-types/stablePhantom.module';
 import { Linear } from './pool-types/linear.module';
 import { SubgraphPoolBase } from '@balancer-labs/sor';
 import { BalancerError, BalancerErrorCode } from '@/balancerErrors';
+import { Interface } from '@ethersproject/abi';
+
+import vaultAbi from '@/lib/abi/Vault.json';
+import { WeightedPoolEncoder } from '@/pool-weighted';
 
 export class Pools {
     constructor(
@@ -45,5 +57,95 @@ export class Pools {
                     BalancerErrorCode.UNSUPPORTED_POOL_TYPE
                 );
         }
+    }
+
+    static encodeJoinPool(params: EncodeJoinPoolInput): string {
+        const vaultLibrary = new Interface(vaultAbi);
+
+        return vaultLibrary.encodeFunctionData('joinPool', [
+            params.poolId,
+            params.sender,
+            params.recipient,
+            params.joinPoolRequest,
+        ]);
+    }
+
+    static constructJoinCall(params: JoinPoolData): string {
+        const {
+            assets,
+            maxAmountsIn,
+            userData,
+            fromInternalBalance,
+            poolId,
+            sender,
+            recipient,
+        } = params;
+
+        const joinPoolRequest: JoinPoolRequest = {
+            assets,
+            maxAmountsIn,
+            userData,
+            fromInternalBalance,
+        };
+
+        const joinPoolInput: EncodeJoinPoolInput = {
+            poolId,
+            sender,
+            recipient,
+            joinPoolRequest,
+        };
+
+        const joinEncoded = Pools.encodeJoinPool(joinPoolInput);
+        return joinEncoded;
+    }
+
+    /**
+     * exactTokensJoinPool Joins user to desired pool with exact tokens in and minimum BPT out based on slippage tolerance
+     * @param {ExactTokensJoinPoolInput} params
+     * @param {string} joiner - Address used to join pool.
+     * @param {string} poolId - Id of pool being joined.
+     * @param {string[]} assets - Array containing addresses of tokens to provide for joining pool. (must have same length and order as amountsIn)
+     * @param {string[]} amountsIn - Array containing amounts of tokens to provide for joining pool. (must have same length and order as assets)
+     * @param {string} expectedBPTOut - Expected amounts of BPT to receive when joining pool.
+     * @param {string} slippage - Slippage to be applied to swap section. i.e. 5%=50000000000000000.
+     * @returns Transaction data with calldata. Outputs.amountsOut has amounts of finalTokensOut returned.
+     */
+    async exactTokensJoinPool(
+        params: ExactTokensJoinPoolInput
+    ): Promise<TransactionData> {
+        const slippageAmountNegative = WeiPerEther.sub(
+            BigNumber.from(params.slippage)
+        );
+        // Set min amounts of BPT out based on slippage
+        const minBPTOut = BigNumber.from(params.expectedBPTOut)
+            .mul(slippageAmountNegative)
+            .div(WeiPerEther)
+            .toString();
+
+        const userData = WeightedPoolEncoder.joinExactTokensInForBPTOut(
+            params.amountsIn,
+            minBPTOut
+        );
+
+        const joinPoolData: JoinPoolData = {
+            assets: params.assets,
+            maxAmountsIn: params.amountsIn,
+            userData,
+            fromInternalBalance: true,
+            poolId: params.poolId,
+            sender: params.joiner,
+            recipient: params.joiner,
+            joinPoolRequest: {} as JoinPoolRequest,
+        };
+
+        const joinCall = Pools.constructJoinCall(joinPoolData);
+
+        // TODO: validate if this is the proper way to build the transaction data even if it is not using the Relayer
+        const joinTransaction: TransactionData = {
+            function: 'joinPool',
+            params: [joinCall],
+        };
+
+        return joinTransaction;
     }
 }
