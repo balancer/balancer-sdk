9b103be6696abe60751e0d119f585b827248b913
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-05-17 19:29:27 -0300

    Create exactTokensJoinPool function
    Add exactTokensJoinPool for Weighted Pools to pools module.

--- balancer-js/src/modules/pools/types.ts
+++ balancer-js/src/modules/pools/types.ts
@@ -1,3 +1,5 @@
+import { JoinPoolRequest } from '@/types';
+
 export enum PoolType {
     Weighted = 'Weighted',
     Investment = 'Investment',
@@ -5,4 +7,22 @@ export enum PoolType {
     MetaStable = 'MetaStable',
     StablePhantom = 'StablePhantom',
     LiquidityBootstrapping = 'LiquidityBootstrapping',
+}
+
+export interface EncodeJoinPoolInput {
+    poolId: string;
+    sender: string;
+    recipient: string;
+    joinPoolRequest: JoinPoolRequest;
+}
+
+export type JoinPoolData = JoinPoolRequest & EncodeJoinPoolInput;
+
+export interface ExactTokensJoinPoolInput {
+    joiner: string;
+    poolId: string;
+    assets: string[];
+    amountsIn: string[];
+    expectedBPTOut: string;
+    slippage: string;
 }
