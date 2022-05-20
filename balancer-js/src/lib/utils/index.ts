6c27396b51ab377a117c4a199b11301a0a0b449b
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-05-20 15:42:12 -0300

    Add utils as helper to calculate slippage

--- balancer-js/src/lib/utils/index.ts
+++ balancer-js/src/lib/utils/index.ts
@@ -1,4 +1,5 @@
 import { getAddress } from '@ethersproject/address';
+import BigNumber from 'bignumber.js';
 
 export * from './errors';
 export * from './permit';
@@ -8,3 +9,8 @@ export * from './aaveHelpers';
 
 export const isSameAddress = (address1: string, address2: string): boolean =>
     getAddress(address1) === getAddress(address2);
+
+export function bnum(val: string | number | BigNumber): BigNumber {
+    const number = typeof val === 'string' ? val : val ? val.toString() : '0';
+    return new BigNumber(number);
+}
---
+++ balancer-js/src/lib/utils/useSlippage.ts
@@ -0,0 +1,55 @@
+import { formatUnits, parseUnits } from '@ethersproject/units';
+import BigNumber from 'bignumber.js';
+
+import { bnum } from '@/lib/utils';
+
+function slippageBasisPoints(slippage: string): string {
+    return bnum(slippage).times(10000).toString();
+}
+
+function minusSlippage(
+    _amount: string,
+    decimals: number,
+    slippage: string
+): string {
+    let amount = parseUnits(_amount, decimals).toString();
+    amount = minusSlippageScaled(amount, slippage);
+
+    return formatUnits(amount, decimals);
+}
+
+function minusSlippageScaled(amount: string, slippage: string): string {
+    const delta = bnum(amount)
+        .times(slippageBasisPoints(slippage))
+        .div(10000)
+        .dp(0, BigNumber.ROUND_UP);
+
+    return bnum(amount).minus(delta).toString();
+}
+
+function addSlippage(
+    _amount: string,
+    decimals: number,
+    slippage: string
+): string {
+    let amount = parseUnits(_amount, decimals).toString();
+    amount = addSlippageScaled(amount, slippage);
+
+    return formatUnits(amount, decimals).toString();
+}
+
+function addSlippageScaled(amount: string, slippage: string): string {
+    const delta = bnum(amount)
+        .times(slippageBasisPoints(slippage))
+        .div(10000)
+        .dp(0, BigNumber.ROUND_DOWN);
+
+    return bnum(amount).plus(delta).toString();
+}
+
+export default {
+    minusSlippage,
+    minusSlippageScaled,
+    addSlippage,
+    addSlippageScaled,
+};
