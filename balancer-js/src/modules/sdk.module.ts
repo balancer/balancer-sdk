75633fd9f2de288ba4a892c47e382d1306bdb083
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-05-25 17:35:46 -0300

    Move join related code into separate join module
    Extract join related code from pools module into join module and refactors code to reuse switch case that routes actions based on pool type

--- balancer-js/src/modules/sdk.module.ts
+++ balancer-js/src/modules/sdk.module.ts
@@ -6,11 +6,13 @@ import { Sor } from './sor/sor.module';
 import { getNetworkConfig } from './sdk.helpers';
 import { Pools } from './pools/pools.module';
 import { Pricing } from './pricing/pricing.module';
+import { Join } from './join/join.module';
 
 export class BalancerSDK {
     public readonly swaps: Swaps;
     public readonly relayer: Relayer;
     public readonly pricing: Pricing;
+    public readonly join: Join;
 
     constructor(
         public config: BalancerSdkConfig,
@@ -21,6 +23,7 @@ export class BalancerSDK {
         this.swaps = new Swaps(this.sor);
         this.relayer = new Relayer(this.swaps);
         this.pricing = new Pricing(config, this.swaps);
+        this.join = new Join(config);
     }
 
     public get networkConfig(): BalancerNetworkConfig {
