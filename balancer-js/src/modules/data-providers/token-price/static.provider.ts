f1ef552fc1ca001c56879ec40af763c3be3fc840
Tim Robinson <tim@timjrobinson.com>
2022-05-26 15:34:40 +1000

    Move USD price calculation to Token Price Provider

--- balancer-js/src/modules/data-providers/token-price/static.provider.ts
+++ balancer-js/src/modules/data-providers/token-price/static.provider.ts
@@ -1,8 +1,46 @@
+import { BigNumber, parseFixed } from '@ethersproject/bignumber';
 import { TokenPriceData } from '@/types';
 import { TokenPriceProvider } from './provider.interface';
 
 export class StaticTokenPriceProvider implements TokenPriceProvider {
-    constructor(private tokenPrices: TokenPriceData[]) {}
+    constructor(private tokenPrices: TokenPriceData[]) {
+        this.calculateUSDPrices();
+    }
+
+    /**
+     * Iterates through all tokens and calculates USD prices
+     * based on data the tokens already have.
+     */
+    calculateUSDPrices(): void {
+        const USDAssets = [
+            '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+            '0x6b175474e89094c44da98b954eedeac495271d0f',
+            '0xdac17f958d2ee523a2206206994597c13d831ec7',
+        ];
+        let assetsAvailable = 0;
+        let assetValueSum = BigNumber.from(0);
+
+        USDAssets.forEach((address) => {
+            const tokenPrice = this.find(address);
+            if (tokenPrice?.ofNativeAsset) {
+                assetValueSum = assetValueSum.add(tokenPrice.ofNativeAsset);
+                assetsAvailable++;
+            }
+        });
+
+        const NativeAssetUSDPrice = assetValueSum.div(assetsAvailable);
+
+        this.tokenPrices = this.tokenPrices.map((tokenPrice) => {
+            if (tokenPrice.ofNativeAsset) {
+                tokenPrice.inUSD = parseFixed('1', 18)
+                    .mul(NativeAssetUSDPrice)
+                    .div(parseFixed(tokenPrice.ofNativeAsset, 18))
+                    .toString();
+            }
+
+            return tokenPrice;
+        });
+    }
 
     find(address: string): TokenPriceData | undefined {
         return this.tokenPrices.find((tokenPrice) => {
--- balancer-js/src/modules/liquidity/liquidity.module.ts
+++ balancer-js/src/modules/liquidity/liquidity.module.ts
@@ -7,39 +7,14 @@ import { PoolProvider } from '../data-providers/pool/provider.interface';
 import { TokenPriceProvider } from '../data-providers/token-price/provider.interface';
 
 export class Liquidity {
-    private pools: PoolProvider;
-    private tokens: TokenProvider;
-    private tokenPrices: TokenPriceProvider;
-
     constructor(
         config: BalancerSdkConfig,
-        poolProvider: PoolProvider,
-        tokenProvider: TokenProvider,
-        tokenPriceProvider: TokenPriceProvider
-    ) {
-        this.pools = poolProvider;
-        this.tokens = tokenProvider;
-        this.tokenPrices = tokenPriceProvider;
-    }
+        private pools: PoolProvider,
+        private tokens: TokenProvider,
+        private tokenPrices: TokenPriceProvider
+    ) {}
 
     async getLiquidity(pool: SubgraphPoolBase): Promise<string> {
-        const USDAssets = ['USDC', 'DAI', 'USDT'];
-        let assetsAvailable = 0;
-        let assetValueSum = BigNumber.from(0);
-
-        USDAssets.forEach((symbol) => {
-            const token = this.tokens.findBy('symbol', symbol);
-            if (!token) return;
-
-            const tokenPrice = this.tokenPrices.find(token.address);
-            if (tokenPrice?.ofNativeAsset) {
-                assetValueSum = assetValueSum.add(tokenPrice.ofNativeAsset);
-                assetsAvailable++;
-            }
-        });
-
-        const NativeAssetUSDPrice = assetValueSum.div(assetsAvailable);
-
         const tokenBalances: TokenBalance[] = pool.tokens.map((token) => {
             const tokenDetails = this.tokens.find(token.address);
             if (!tokenDetails) {
@@ -48,24 +23,15 @@ export class Liquidity {
                 );
             }
 
-            let price: TokenPrice | undefined;
             const tokenPrice = this.tokenPrices.find(tokenDetails.address);
-            if (tokenPrice?.ofNativeAsset) {
-                price = {
-                    inUSD: parseFixed('1', 18)
-                        .mul(NativeAssetUSDPrice)
-                        .div(parseFixed(tokenPrice?.ofNativeAsset || '1', 18))
-                        .toString(),
-                    ofNativeAsset: tokenPrice.ofNativeAsset,
-                };
-            }
-
             const tokenBalance: TokenBalance = {
                 token: {
                     address: token.address,
                     decimals: token.decimals,
                     priceRate: token.priceRate,
-                    price: price,
+                    price: {
+                        inUSD: tokenPrice?.inUSD,
+                    },
                 },
                 balance: token.balance,
                 weight: token.weight
