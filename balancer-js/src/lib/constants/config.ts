3d3aeda4267a4f99b911ba896fd91f65ed7935db
Eduardo Pelitti <eduardo@underscope.io>
2022-05-29 20:14:21 -0300

    update subgraph and package version (#2)
    * update subgraph and package version
    
    * update config constants with new subgraph url

--- balancer-js/src/lib/constants/config.ts
+++ balancer-js/src/lib/constants/config.ts
@@ -82,7 +82,7 @@ export const BALANCER_NETWORK_CONFIG: Record<Network, BalancerNetworkConfig> = {
         },
         urls: {
             subgraph:
-                'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-kovan-v2',
+                'https://api.thegraph.com/subgraphs/name/aleelus/pools-diss',
         },
         pools: {
             bbausd: {
