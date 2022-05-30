67e33eaff274ffbf51db250b46abf1550e6f6cb1
Tim Robinson <tim@timjrobinson.com>
2022-05-30 16:39:56 +1000

    Add more weighted pool tests

--- balancer-js/src/modules/liquidity/liquidity.module.spec.ts
+++ balancer-js/src/modules/liquidity/liquidity.module.spec.ts
@@ -32,9 +32,54 @@ beforeEach(() => {
     );
 });
 
-describe('Weighted Pool Liquidity Concern', () => {
-    it('Correctly calculates value of a 60/40 pool', async () => {
-        const liquidity = await liquidityProvider.getLiquidity(pools[0]);
-        expect(liquidity).to.be.eq('10000.0');
+function findPool(address: string): SubgraphPoolBase {
+    const pool = pools.find((pool) => {
+        return pool.address === address;
     });
+    if (!pool)
+        throw new Error('Could not find test pool of address: ' + address);
+    return pool;
+}
+
+describe('Liquidity Module', () => {
+    context('Weighted Pool Calculations', () => {
+        it('Correct calculates liquidity of a 50/50 WBTC/WETH pool', async () => {
+            const liquidity = await liquidityProvider.getLiquidity(
+                findPool('0xa6f548df93de924d73be7d25dc02554c6bd66db5')
+            );
+            expect(liquidity).to.be.eq('640000.0');
+        });
+
+        it('Correct calculates liquidity of a 60/40 pool', async () => {
+            const liquidity = await liquidityProvider.getLiquidity(
+                findPool('0xc6a5032dc4bf638e15b4a66bc718ba7ba474ff73')
+            );
+            expect(liquidity).to.be.eq('10000.0');
+        });
+
+        it('Correctly calculates value of a 25/25/25/25 pool which is slightly off weight', async () => {
+            const liquidity = await liquidityProvider.getLiquidity(
+                findPool('0xd8833594420db3d6589c1098dbdd073f52419dba')
+            );
+            expect(liquidity).to.be.eq('127080.0');
+        });
+    });
+
+    // context('Stable Pool calculations', () => {
+    //     it('Correctly calculates value of a USD 3pool', async () => {
+    //         const liquidity = await liquidityProvider.getLiquidity(pools[2]);
+    //         expect(liquidity).to.be.eq('30000.0');
+    //     });
+    // });
+
+    // context('Metastable Pool calculations', () => {
+    //     it('Correct calculates liquidity of a wstETH/ETH metastable pool', async () => {
+    //         const liquidity = await liquidityProvider.getLiquidity(pools[2]);
+    //         expect(liquidity).to.be.eq('30000.0');
+    //     });
+    // });
+
+    // context('PhantomStable Pool calculations', () => {
+    //     it('Correctly calculates liquidity of a Boosted USD 3pool', async () => {});
+    // });
 });
