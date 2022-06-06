4e720ed91b6a0e09c79753e7a0243eed0b8db3b1
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-06-06 12:01:48 -0300

    Replace impersonate on join pool integration test
    Hardhat user impersonation has an open issue that is preventing the integration test to pass, so the approach was replaced by manually setting balances on the default user, approving tokens for the selected vault and then joining pool.

--- balancer-js/src/modules/pools/join/join.module.integration.spec.ts
+++ balancer-js/src/modules/pools/join/join.module.integration.spec.ts
@@ -2,13 +2,18 @@ import dotenv from 'dotenv';
 import { expect } from 'chai';
 import { Network } from '@/.';
 import { MockPoolDataService } from '@/test/lib/mockPool';
-import hardhat from 'hardhat';
-import { JsonRpcProvider, TransactionReceipt } from '@ethersproject/providers';
-import { BigNumber } from 'ethers';
 import { B_50WBTC_50WETH, getForkedPools } from '@/test/lib/mainnetPools';
+import hardhat from 'hardhat';
+
+import { JsonRpcProvider, TransactionReceipt } from '@ethersproject/providers';
+import { Contract } from '@ethersproject/contracts';
+import { BigNumber } from 'ethers';
+import { parseFixed } from '@ethersproject/bignumber';
+
 import { Pools } from '../pools.module';
 import { balancerVault } from '@/lib/constants/config';
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import erc20Abi from '@/lib/abi/erc20.json';
+import { SubgraphToken } from '@balancer-labs/sor';
 
 dotenv.config();
 
@@ -25,32 +30,53 @@ const getERC20Contract = (address: string) => {
 
 const rpcUrl = 'http://127.0.0.1:8545';
 const provider = new ethers.providers.JsonRpcProvider(rpcUrl, 1);
+const signer = provider.getSigner();
 
-const IMPERSONATED_ADDRESS = '0xf933058d90020e88ae97fa5a4753ff9b4f3a7879';
-let signer: SignerWithAddress;
+const wETH = B_50WBTC_50WETH.tokens[0];
+const wBTC = B_50WBTC_50WETH.tokens[1];
+// Slots used to set the account balance for each token through hardhat_setStorageAt
+// Info fetched using npm package slot20
+const wETH_SLOT = 3;
+const wBTC_SLOT = 0;
+
+const tokensIn = [wETH, wBTC];
+const tokensInAddresses = tokensIn.map((token) => token.address);
+const amountsIn = ['0.018174', '0.001099'];
 
 // Setup
 
-const setupSigner = async (address: string): Promise<SignerWithAddress> => {
-  await hardhat.network.provider.request({
-    method: 'hardhat_impersonateAccount',
-    params: [address],
-  });
-  return ethers.getSigner(address);
-};
+const setupTokenBalance = async (
+  signerAddress: string,
+  tokenAddress: string,
+  slot: number
+) => {
+  const toBytes32 = (bn: BigNumber) => {
+    return ethers.utils.hexlify(ethers.utils.zeroPad(bn.toHexString(), 32));
+  };
 
-const resetSigner = async (address: string) => {
-  await hardhat.network.provider.request({
-    method: 'hardhat_stopImpersonatingAccount',
-    params: [address],
-  });
-};
+  const setStorageAt = async (
+    tokenAddress: string,
+    index: string,
+    value: string
+  ) => {
+    await provider.send('hardhat_setStorageAt', [tokenAddress, index, value]);
+    await provider.send('evm_mine', []); // Just mines to the next block
+  };
 
-const setupBalance = async (address: string) => {
-  await hardhat.network.provider.send('hardhat_setBalance', [
-    address,
-    '0x1000000000000000000',
-  ]);
+  const locallyManipulatedBalance = parseFixed('100000', 18);
+
+  // Get storage slot index
+  const index = ethers.utils.solidityKeccak256(
+    ['uint256', 'uint256'],
+    [signerAddress, slot] // key, slot
+  );
+
+  // Manipulate local balance (needs to be bytes32 string)
+  await setStorageAt(
+    tokenAddress,
+    index,
+    toBytes32(locallyManipulatedBalance).toString()
+  );
 };
 
 const setupPoolsModule = async (provider: JsonRpcProvider) => {
@@ -68,13 +94,23 @@ const setupPoolsModule = async (provider: JsonRpcProvider) => {
   return poolsModule;
 };
 
+const approveTokens = async (tokens: SubgraphToken[], amounts: string[]) => {
+  const parsedAmounts = amounts.map((amount, i) => {
+    return parseFixed(amount, tokens[i].decimals);
+  });
+  for (let i = 0; i < tokens.length; i++) {
+    const tokenContract = new Contract(tokens[i].address, erc20Abi, signer);
+    await tokenContract.approve(balancerVault, parsedAmounts[i]);
+  }
+};
+
 // Test scenarios
 
 describe('join execution', async () => {
   let poolsModule: Pools;
   let transactionReceipt: TransactionReceipt;
-  let balanceBefore: BigNumber;
-  let balanceExpected: BigNumber;
+  let bptBalanceBefore: BigNumber;
+  let bptBalanceIncrease: BigNumber;
   let signerAddress: string;
   const bptContract = getERC20Contract(B_50WBTC_50WETH.address);
 
@@ -89,36 +125,31 @@ describe('join execution', async () => {
         },
       },
     ]);
-    signer = await setupSigner(IMPERSONATED_ADDRESS);
+    poolsModule = await setupPoolsModule(provider); // must setup pools module before amountsIn calculation
     signerAddress = await signer.getAddress();
-    await setupBalance(signerAddress);
-    poolsModule = await setupPoolsModule(provider);
+    await setupTokenBalance(signerAddress, wETH.address, wETH_SLOT);
+    await setupTokenBalance(signerAddress, wBTC.address, wBTC_SLOT);
+    await approveTokens(tokensIn, amountsIn);
   });
 
   context('exactTokensInJoinPool transaction', () => {
     before(async function () {
       this.timeout(20000);
 
-      balanceBefore = await bptContract.balanceOf(signerAddress);
-      const wETH = B_50WBTC_50WETH.tokens[0];
-      const wBTC = B_50WBTC_50WETH.tokens[1];
-      const tokensIn = [wETH.address, wBTC.address];
-      const amountsIn = [
-        (parseInt(wETH.balance) / 100).toString(),
-        (parseInt(wBTC.balance) / 100).toString(),
-      ];
+      bptBalanceBefore = await bptContract.balanceOf(signerAddress);
+
       const slippage = '0.01';
       const data = await poolsModule.join.encodedExactTokensInJoinPool(
         signerAddress,
         B_50WBTC_50WETH.id,
-        tokensIn,
+        tokensInAddresses,
         amountsIn,
         slippage
       );
       const to = balancerVault;
-      const tx = { data, to };
+      const tx = { data, to }; // , gasPrice: '60000000000', gasLimit: '2000000'
 
-      balanceExpected = BigNumber.from('1'); // setting 1 to force test to fail - correct value will be available when code works as expected
+      bptBalanceIncrease = BigNumber.from('8793334702586135'); // get from bptOut value (after adapting function to return transaction attributes)
       transactionReceipt = await (await signer.sendTransaction(tx)).wait();
     });
 
@@ -127,18 +158,13 @@ describe('join execution', async () => {
     });
 
     it('balance should increase', async () => {
-      const balanceAfter: BigNumber = await bptContract.balanceOf(
+      const bptBalanceAfter: BigNumber = await bptContract.balanceOf(
         signerAddress
       );
 
-      expect(balanceAfter.sub(balanceBefore).toNumber()).to.eql(
-        balanceExpected.toNumber()
+      expect(bptBalanceAfter.sub(bptBalanceBefore).toNumber()).to.eql(
+        bptBalanceIncrease.toNumber()
       );
-    });
-
-    after(async () => {
-      await resetSigner(IMPERSONATED_ADDRESS);
-      await signer.getAddress();
     });
   });
 }).timeout(20000);
