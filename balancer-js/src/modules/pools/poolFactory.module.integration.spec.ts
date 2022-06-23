b82798acd6464c328058d3e663f2fcabf07d6e06
Javier Gonzalez <javier.e.glz@gmail.com>
2022-06-23 09:54:38 +0200

    formatting

--- balancer-js/src/modules/pools/poolFactory.module.integration.spec.ts
+++ balancer-js/src/modules/pools/poolFactory.module.integration.spec.ts
@@ -1,16 +1,11 @@
 import dotenv from 'dotenv';
 import { expect } from 'chai';
-import {
-    BalancerSdkConfig,
-    Network,
-    BalancerSDK,
-    SeedToken,
-} from '@/.';
+import { BalancerSdkConfig, BalancerSDK, SeedToken } from '@/.';
 import { ADDRESSES } from '@/test/lib/constants';
-import { TransactionResponse } from '@ethersproject/providers';
 import { BigNumber, Contract, ethers } from 'ethers';
 
 dotenv.config();
+
 const { ALCHEMY_URL: jsonRpcUrl } = process.env;
 
 const getFactoryContract = (address: string) => {
@@ -26,90 +21,140 @@ const provider = new ethers.providers.JsonRpcProvider(rpcUrl, 1);
 const signer = provider.getSigner();
 
 const sdkConfig: BalancerSdkConfig = {
-    network: 1,
-    rpcUrl: rpcUrl,
+  network: 1,
+  rpcUrl: rpcUrl,
 };
 
-const SEED_TOKENS: Array<SeedToken> = [ 
-    { id: 0, tokenAddress: ADDRESSES[42].DAI.address, weight: 30, isLocked: false, amount: "200000000" }, 
-    { id: 1, tokenAddress: ADDRESSES[42].USDC.address, weight: 40, isLocked: false, amount: "200000000" },
-    { id: 2, tokenAddress: ADDRESSES[42].WBTC.address, weight: 30, isLocked: false, amount: "200000000" } 
+const SEED_TOKENS: Array<SeedToken> = [
+  {
+    id: 0,
+    tokenAddress: ADDRESSES[42].DAI.address,
+    weight: 30,
+    isLocked: false,
+    amount: '200000000',
+  },
+  {
+    id: 1,
+    tokenAddress: ADDRESSES[42].USDC.address,
+    weight: 40,
+    isLocked: false,
+    amount: '200000000',
+  },
+  {
+    id: 2,
+    tokenAddress: ADDRESSES[42].WBTC.address,
+    weight: 30,
+    isLocked: false,
+    amount: '200000000',
+  },
 ];
 
 const INIT_JOIN_PARAMS = {
-    poolId: 200,
-    sender: "0x0000000000000000000000000000000000000001",
-    receiver: "0x0000000000000000000000000000000000000002",
-    tokenAddresses: [ADDRESSES[42].DAI.address, ADDRESSES[42].USDC.address, ADDRESSES[42].WBTC.address],
-    initialBalancesString: ["2000000000000000000", "2000000000000000000", "2000000000000000000"],
-}
+  poolId: 200,
+  sender: '0x0000000000000000000000000000000000000001',
+  receiver: '0x0000000000000000000000000000000000000002',
+  tokenAddresses: [
+    ADDRESSES[42].DAI.address,
+    ADDRESSES[42].USDC.address,
+    ADDRESSES[42].WBTC.address,
+  ],
+  initialBalancesString: [
+    '2000000000000000000',
+    '2000000000000000000',
+    '2000000000000000000',
+  ],
+};
 
 const POOL_PARAMS = {
-    name: "WeightedPoolFactory",
-    symbol: "WPOOL",
-    initialFee: "0.1",
-    seedTokens: SEED_TOKENS,
-    owner: "0x0000000000000000000000000000000000000001",
-    value: "0.1",
-}
-const poolFactoryContract = ""
-const vaultAddress = ""
+  name: 'WeightedPoolFactory',
+  symbol: 'WPOOL',
+  initialFee: '0.1',
+  seedTokens: SEED_TOKENS,
+  owner: '0x0000000000000000000000000000000000000001',
+  value: '0.1',
+};
+const poolFactoryContract = '';
 const getERC20Contract = (address: string) => {
-    return new ethers.Contract(
-      address,
-      ['function balanceOf(address) view returns (uint256)', 'function name() returns (string)'],
-      provider
-    );
-  };
+  return new ethers.Contract(
+    address,
+    [
+      'function balanceOf(address) view returns (uint256)',
+      'function name() returns (string)',
+    ],
+    provider
+  );
+};
 
 describe('pool factory module', () => {
-    context('factory transaction', () => {
-        let balancer: BalancerSDK, to: string, data:string, value:BigNumber,
-            expectedPoolName: string, transactionReceipt: ethers.providers.TransactionReceipt
-        beforeEach(async () => {
-            balancer = new BalancerSDK(sdkConfig);
-            let createTx =  await balancer.pools.weighted.buildCreateTx(POOL_PARAMS);
-            to = createTx.to
-            data = createTx.data
-            value = createTx.value as BigNumber
-            expectedPoolName = createTx.attributes.name
-            const tx = { to, data, value };
-            transactionReceipt = await (await signer.sendTransaction(tx)).wait();
-        })
+  // Setup chain
+  before(async function () {
+    this.timeout(20000);
 
-        it('should send the transaction succesfully', async () => {
-            expect(transactionReceipt.status).to.eql(1);
-        });
+    await provider.send('hardhat_reset', [
+      {
+        forking: {
+          jsonRpcUrl,
+        },
+      },
+    ]);
+  });
 
-        it('should create a pool with the correct token name', async () => {
-            const abi = [
-                "event PoolCreated(address indexed)"
-            ];
-            const contract = new Contract(poolFactoryContract, abi, provider);
-            const { address } = contract.filters.PoolCreated()
-            const tokenContract = getERC20Contract(address as string)
-            expect(expectedPoolName).to.eql(await tokenContract.getName())
-        })
-    })
-    context('initial join transaction', () => {
-        let balancer: BalancerSDK, transactionReceipt: ethers.providers.TransactionReceipt
-        beforeEach(async () => {
-            balancer = new BalancerSDK(sdkConfig);
-            const txAttributes =  await balancer.pools.weighted.buildCreateTx(POOL_PARAMS);
-            transactionReceipt = await (await signer.sendTransaction(txAttributes)).wait();
-        })
+  context('factory transaction', () => {
+    let balancer: BalancerSDK,
+      to: string,
+      data: string,
+      value: BigNumber,
+      expectedPoolName: string,
+      transactionReceipt: ethers.providers.TransactionReceipt;
+    beforeEach(async () => {
+      balancer = new BalancerSDK(sdkConfig);
+      const createTx = await balancer.pools.weighted.buildCreateTx(POOL_PARAMS);
+      to = createTx.to;
+      data = createTx.data;
+      value = createTx.value as BigNumber;
+      expectedPoolName = createTx.attributes.name;
+      const tx = { to, data, value };
+      transactionReceipt = await (await signer.sendTransaction(tx)).wait();
+    });
 
-        it('should send the transaction succesfully', async () => {
-            expect(transactionReceipt.status).to.eql(1);
-        });
-        
-        it('should give user tokens on initial join', async () => {
-            const { address } = await balancer.pools.getPoolInfoFromCreateTx(transactionReceipt)
-            const tx = await balancer.pools.weighted.buildInitJoin(INIT_JOIN_PARAMS); 
-            await (await signer.sendTransaction(tx)).wait();
-            const createdPool = getERC20Contract(address)
-            const senderBalance: BigNumber = await createdPool.balanceOf(signer)
-            expect(senderBalance.gt(BigNumber.from("0"))).to.eql(true)
-        })
-    })
+    it('should send the transaction succesfully', async () => {
+      expect(transactionReceipt.status).to.eql(1);
+    });
+
+    it('should create a pool with the correct token name', async () => {
+      const abi = ['event PoolCreated(address indexed)'];
+      const contract = new Contract(poolFactoryContract, abi, provider);
+      const { address } = contract.filters.PoolCreated();
+      const tokenContract = getERC20Contract(address as string);
+      expect(expectedPoolName).to.eql(await tokenContract.getName());
+    });
+  });
+  context('initial join transaction', () => {
+    let balancer: BalancerSDK,
+      transactionReceipt: ethers.providers.TransactionReceipt;
+    beforeEach(async () => {
+      balancer = new BalancerSDK(sdkConfig);
+      const txAttributes = await balancer.pools.weighted.buildCreateTx(
+        POOL_PARAMS
+      );
+      transactionReceipt = await (
+        await signer.sendTransaction(txAttributes)
+      ).wait();
+    });
+
+    it('should send the transaction succesfully', async () => {
+      expect(transactionReceipt.status).to.eql(1);
+    });
+
+    it('should give user tokens on initial join', async () => {
+      const { address } = await balancer.pools.getPoolInfoFromCreateTx(
+        transactionReceipt
+      );
+      const tx = await balancer.pools.weighted.buildInitJoin(INIT_JOIN_PARAMS);
+      await (await signer.sendTransaction(tx)).wait();
+      const createdPool = getERC20Contract(address);
+      const senderBalance: BigNumber = await createdPool.balanceOf(signer);
+      expect(senderBalance.gt(BigNumber.from('0'))).to.eql(true);
+    });
+  });
 });
