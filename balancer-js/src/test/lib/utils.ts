d7dae9de2d51d6eaf049dba458d993ff6829d34b
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-07-11 11:34:25 -0300

    Fix join example signer/provider mismatch

--- balancer-js/src/test/lib/utils.ts
+++ balancer-js/src/test/lib/utils.ts
@@ -1,20 +1,31 @@
 import { BalancerSDK } from '@/.';
-import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers';
+import { JsonRpcSigner } from '@ethersproject/providers';
 import { BigNumber } from '@ethersproject/bignumber';
 import { balancerVault } from '@/lib/constants/config';
 import { hexlify, zeroPad } from '@ethersproject/bytes';
 import { keccak256 } from '@ethersproject/solidity';
 
+/**
+ * Setup local fork with approved token balance for a given account
+ *
+ * @param {BalancerSDK}   balancer Balancer SDK required to fetch contracts
+ * @param {JsonRpcSigner} signer Account that will have token balance set and approved
+ * @param {string[]}      tokens Token addresses which balance will be set and approved
+ * @param {number[]}      slots Slot that stores token balance in memory - use npm package `slot20` to identify which slot to provide
+ * @param {string[]}      balances Balances in EVM amounts
+ * @param {string}        jsonRpcUrl Url with remote node to be forked locally
+ * @param {number}        blockNumber Number of the block that the fork will happen
+ */
 export const forkSetup = async (
   balancer: BalancerSDK,
-  provider: JsonRpcProvider,
+  signer: JsonRpcSigner,
   tokens: string[],
   slots: number[],
   balances: string[],
   jsonRpcUrl: string,
   blockNumber?: number
 ): Promise<void> => {
-  await provider.send('hardhat_reset', [
+  await signer.provider.send('hardhat_reset', [
     {
       forking: {
         jsonRpcUrl,
@@ -25,14 +36,9 @@ export const forkSetup = async (
 
   for (let i = 0; i < tokens.length; i++) {
     // Set initial account balance for each token that will be used to join pool
-    await setTokenBalance(
-      provider.getSigner(),
-      tokens[i],
-      slots[i],
-      balances[i]
-    );
+    await setTokenBalance(signer, tokens[i], slots[i], balances[i]);
     // Approve appropriate allowances so that vault contract can move tokens
-    await approveToken(balancer, tokens[i], balances[i], provider.getSigner());
+    await approveToken(balancer, tokens[i], balances[i], signer);
   }
 };
 
