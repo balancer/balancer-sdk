f4a4e617404166999fa36a1308f9cb05f3008b8f
bronco <rg3mvdb0@protonmail.com>
2022-07-16 11:58:54 +0200

    liquidity gauges data

--- balancer-js/src/modules/data/index.ts
+++ balancer-js/src/modules/data/index.ts
@@ -1,5 +1,6 @@
 export * as balEmissions from './bal/emissions';
 export * from './gauge-controller/multicall';
+export * from './liquidity-gauges';
 export * from './pool';
 export * from './token';
 export * from './token-prices';
