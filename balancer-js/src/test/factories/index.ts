51f29b0a2609c89a0366a53b72a2683fabbbffde
bronco <rg3mvdb0@protonmail.com>
2022-07-17 18:13:01 +0200

    data factories

--- balancer-js/src/test/factories/index.ts
+++ balancer-js/src/test/factories/index.ts
@@ -1,6 +1,7 @@
 import * as sor from './sor';
 import * as sdk from './sdk';
+import * as data from './data';
 
-const factories = { ...sor, ...sdk };
+const factories = { ...sor, ...sdk, data };
 
 export { factories };
