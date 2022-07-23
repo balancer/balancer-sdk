f7d8b1ea4ab1ed341daa1bef2225d77138a71bcf
bronco <rg3mvdb0@protonmail.com>
2022-07-23 20:21:17 +0200

    wip: bbaUsd flow

---
+++ balancer-js/src/modules/zaps/bbausd2-migrations/bbausd1.ts
@@ -0,0 +1,257 @@
+import { ADDRESSES } from './addresses';
+import { Relayer } from '@/modules/relayer/relayer.module';
+import { BatchSwapStep, FundManagement, SwapType } from '@/modules/swaps/types';
+import { Interface } from '@ethersproject/abi';
+import { MaxUint256, MaxInt256, Zero } from '@ethersproject/constants';
+// TODO - Ask Nico to update Typechain?
+import balancerRelayerAbi from '@/lib/abi/BalancerRelayer.json';
+import { PoolToken } from '@/types';
+import { BigNumber } from '@ethersproject/bignumber';
+const balancerRelayerInterface = new Interface(balancerRelayerAbi);
+
+const SWAP_RESULT_BBAUSD = Relayer.toChainedReference('24');
+
+export class BbaUsd1Builder {
+  private addresses;
+
+  constructor(networkId: 1 | 5) {
+    this.addresses = ADDRESSES[networkId];
+  }
+
+  calldata(
+    amount: string,
+    expectedAmount = MaxInt256.toString(),
+    userAddress: string,
+    staked: boolean,
+    authorisation: string,
+    tokens: PoolToken[]
+  ): {
+    to: string;
+    data: string;
+  } {
+    const relayer = this.addresses.relayer;
+    let calls: string[] = [];
+
+    if (staked) {
+      calls = [
+        this.buildSetRelayerApproval(authorisation),
+        this.buildWithdraw(userAddress, amount),
+        this.buildSwap(amount, expectedAmount, relayer, tokens),
+        this.buildDeposit(userAddress),
+      ];
+    } else {
+      calls = [
+        this.buildSetRelayerApproval(authorisation),
+        this.buildSwap(amount, expectedAmount, userAddress, tokens),
+      ];
+    }
+
+    const callData = balancerRelayerInterface.encodeFunctionData('multicall', [
+      calls,
+    ]);
+
+    return {
+      to: this.addresses.relayer,
+      data: callData,
+    };
+  }
+
+  /**
+   * Creates encoded batchSwap function to swap Linear BPTs to underlying stables.
+   * outputreferences should contain the amounts of each new Linear BPT.
+   *
+   * @returns BatchSwap call.
+   */
+  buildSwap(
+    bptAmount: string,
+    expectedBptReturn: string,
+    recipient: string,
+    tokens: PoolToken[]
+  ): string {
+    const assets = [
+      this.addresses.bbausd2.address,
+      this.addresses.DAI,
+      this.addresses.linearDai1.address,
+      this.addresses.linearDai2.address,
+      this.addresses.USDC,
+      this.addresses.linearUsdc1.address,
+      this.addresses.linearUsdc2.address,
+      this.addresses.USDT,
+      this.addresses.linearUsdt1.address,
+      this.addresses.linearUsdt2.address,
+    ];
+
+    const outputReferences = [{ index: 0, key: SWAP_RESULT_BBAUSD }];
+
+    // Calculate proportional token amounts
+
+    // Assuming 1:1 exchange rates between tokens
+    // TODO: Fetch current prices, or use price or priceRate from subgraph?
+    const totalLiquidity = tokens.reduce(
+      (sum, token) => sum.add(BigNumber.from(token.balance)),
+      Zero
+    );
+
+    const weights = Object.fromEntries(
+      tokens.map((token) => [
+        token.symbol,
+        BigNumber.from(token.balance).div(totalLiquidity),
+      ])
+    );
+
+    // bbausd1[bbausd1]blinear1[linear1]stable[linear2]blinear2[bbausd2]bbausd2 and then do that proportionally for each underlying stable.
+    // TO DO - Will swap order matter here? John to ask Fernando.
+
+    // Split BPT amount proportionally:
+    const daiBptAmt = BigNumber.from(bptAmount)
+      .mul(weights['bb-a-DAI'])
+      .toString();
+    const usdcBptAmt = BigNumber.from(bptAmount)
+      .mul(weights['bb-a-USDC'])
+      .toString();
+    const usdtBptAmt = BigNumber.from(bptAmount)
+      .mul(weights['bb-a-USDT'])
+      .toString();
+
+    const swaps: BatchSwapStep[] = [
+      {
+        poolId: this.addresses.linearDai1.id,
+        assetInIndex: 2,
+        assetOutIndex: 1,
+        amount: daiBptAmt,
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.linearDai2.id,
+        assetInIndex: 1,
+        assetOutIndex: 3,
+        amount: '0',
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.bbausd2.id,
+        assetInIndex: 3,
+        assetOutIndex: 0,
+        amount: '0',
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.linearUsdc1.id,
+        assetInIndex: 5,
+        assetOutIndex: 4,
+        amount: usdcBptAmt,
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.linearUsdc2.id,
+        assetInIndex: 4,
+        assetOutIndex: 6,
+        amount: '0',
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.bbausd2.id,
+        assetInIndex: 6,
+        assetOutIndex: 0,
+        amount: '0',
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.linearUsdt1.id,
+        assetInIndex: 8,
+        assetOutIndex: 7,
+        amount: usdtBptAmt,
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.linearUsdt2.id,
+        assetInIndex: 7,
+        assetOutIndex: 9,
+        amount: '0',
+        userData: '0x',
+      },
+      {
+        poolId: this.addresses.bbausd2.id,
+        assetInIndex: 9,
+        assetOutIndex: 0,
+        amount: '0',
+        userData: '0x',
+      },
+    ];
+
+    // For now assuming ref amounts will be safe - should we add more accurate?
+    const limits = [
+      expectedBptReturn.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+      MaxInt256.toString(),
+    ];
+
+    // Swap to/from Relayer
+    const funds: FundManagement = {
+      sender: this.addresses.relayer,
+      recipient,
+      fromInternalBalance: true,
+      toInternalBalance: false,
+    };
+
+    const encodedBatchSwap = Relayer.encodeBatchSwap({
+      swapType: SwapType.SwapExactIn,
+      swaps,
+      assets,
+      funds,
+      limits,
+      deadline: MaxUint256,
+      value: '0',
+      outputReferences,
+    });
+
+    return encodedBatchSwap;
+  }
+
+  /**
+   * Is using gauge relayer to withdraw staked BPT from user to itself
+   *
+   * @returns withdraw call
+   */
+  buildWithdraw(sender: string, amount: string): string {
+    return Relayer.encodeGaugeWithdraw(
+      this.addresses.staBal3.gauge,
+      sender,
+      this.addresses.relayer,
+      amount
+    );
+  }
+
+  /**
+   * Is using gauge relayer to deposit user's BPT to itself
+   *
+   * @returns deposit call
+   */
+  buildDeposit(recipient: string): string {
+    return Relayer.encodeGaugeDeposit(
+      this.addresses.bbausd2.gauge,
+      this.addresses.relayer,
+      recipient,
+      SWAP_RESULT_BBAUSD.toString()
+    );
+  }
+
+  buildApproveVault(token: string): string {
+    return Relayer.encodeApproveVault(token, MaxUint256.toString());
+  }
+
+  buildSetRelayerApproval(authorisation: string): string {
+    return Relayer.encodeSetRelayerApproval(
+      this.addresses.relayer,
+      true,
+      authorisation
+    );
+  }
+}
--- balancer-js/src/modules/zaps/migrations.ts
+++ balancer-js/src/modules/zaps/migrations.ts
@@ -1,6 +1,8 @@
 import { defaultAbiCoder } from '@ethersproject/abi';
 import { MaxInt256 } from '@ethersproject/constants';
 import { StaBal3Builder } from './bbausd2-migrations/stabal3';
+import { BbaUsd1Builder } from './bbausd2-migrations/bbausd1';
+import { PoolToken } from '@/types';
 
 export class Migrations {
   constructor(private network: 1 | 5) {}
@@ -19,6 +21,32 @@ export class Migrations {
       userAddress,
       staked,
       authorisation
+    );
+
+    return {
+      to: request.to,
+      data: request.data,
+      decode: (output) =>
+        defaultAbiCoder.decode(['int256[]'], output[2])[3].toString(),
+    };
+  }
+
+  bbaUsd(
+    userAddress: string,
+    amount: string,
+    limit = MaxInt256.toString(),
+    authorisation: string,
+    staked: boolean,
+    tokens: PoolToken[]
+  ): { to: string; data: string; decode: (output: string) => string } {
+    const builder = new BbaUsd1Builder(this.network);
+    const request = builder.calldata(
+      amount,
+      limit,
+      userAddress,
+      staked,
+      authorisation,
+      tokens
     );
 
     return {
