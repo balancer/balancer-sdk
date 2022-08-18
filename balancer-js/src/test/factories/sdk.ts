a33b7f393ba1e9323f05af9b7b5a1703dd7c4edf
Tim Robinson <tim@timjrobinson.com>
2022-08-18 12:41:41 +1000

    Fix test factories to match types

--- balancer-js/src/test/factories/sdk.ts
+++ balancer-js/src/test/factories/sdk.ts
@@ -26,6 +26,7 @@ const poolFactory = Factory.define<Pool>(({ params, afterBuild }) => {
 
   return {
     id: '0xa6f548df93de924d73be7d25dc02554c6bd66db500020000000000000000000e',
+    name: 'Test Pool',
     address: '0xa6f548df93de924d73be7d25dc02554c6bd66db5',
     poolType: PoolType.Weighted,
     swapFee: '0.001',
@@ -34,6 +35,7 @@ const poolFactory = Factory.define<Pool>(({ params, afterBuild }) => {
     tokensList: [],
     totalWeight: '1',
     totalShares: '1',
+    totalLiquidity: '0',
   };
 });
 
