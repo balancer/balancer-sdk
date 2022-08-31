9c7877c93f141c4978e881254f39ef1e2044b1b5
bronco <rg3mvdb0@protonmail.com>
2022-08-31 15:27:46 +0200

    fee collector data

--- balancer-js/src/modules/data/index.ts
+++ balancer-js/src/modules/data/index.ts
@@ -5,5 +5,6 @@ export * from './pool';
 export * from './token';
 export * from './token-prices';
 export * from './fee-distributor/repository';
+export * from './fee-collector/repository';
 export * from './token-yields/repository';
 export * from './block-number';
