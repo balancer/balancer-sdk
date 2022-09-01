28442822ed9e0f581cf9172a09b5ff3dc4c965b6
Tim Robinson <tim@timjrobinson.com>
2022-09-01 18:43:14 +1000

    Fix merge issues

--- balancer-js/src/modules/sdk.helpers.ts
+++ balancer-js/src/modules/sdk.helpers.ts
@@ -1,22 +1,5 @@
 import { BALANCER_NETWORK_CONFIG } from '@/lib/constants/config';
-import {
-  BalancerDataRepositories,
-  BalancerNetworkConfig,
-  BalancerSdkConfig,
-} from '@/types';
-import {
-  BlockNumberRepository,
-  CoingeckoPriceRepository,
-  LiquidityGaugeSubgraphRPCProvider,
-  PoolsSubgraphRepository,
-  StaticTokenProvider,
-  FeeDistributorRepository,
-  FeeCollectorRepository,
-  TokenYieldsRepository,
-} from './data';
-import { Sor } from './sor/sor.module';
-import initialCoingeckoList from '@/modules/data/token-prices/initial-list.json';
-
+import { BalancerNetworkConfig, BalancerSdkConfig } from '@/types';
 export function getNetworkConfig(
   config: BalancerSdkConfig
 ): BalancerNetworkConfig {
@@ -39,53 +22,4 @@ export function getNetworkConfig(
       subgraph: config.customSubgraphUrl ?? config.network.urls.subgraph,
     },
   };
-}
-
-export function getDataRepositories(
-  config: BalancerSdkConfig
-): BalancerDataRepositories {
-  const networkConfig = getNetworkConfig(config);
-  const blockDayAgo = () => {
-    return new BlockNumberRepository(
-      networkConfig.urls.blockNumberSubgraph || ''
-    ).find('dayAgo');
-  };
-  // const tokenAddresses = [];
-  const tokenAddresses = initialCoingeckoList
-    .filter((t) => t.chainId == networkConfig.chainId)
-    .map((t) => t.address);
-  const sor = new Sor(config);
-  const repositories = {
-    pools: new PoolsSubgraphRepository(networkConfig.urls.subgraph),
-    yesterdaysPools: new PoolsSubgraphRepository(
-      networkConfig.urls.subgraph,
-      blockDayAgo
-    ),
-    tokenPrices: new CoingeckoPriceRepository(
-      tokenAddresses,
-      networkConfig.chainId
-    ),
-    tokenMeta: new StaticTokenProvider([]),
-    liquidityGauges: new LiquidityGaugeSubgraphRPCProvider(
-      networkConfig.urls.gaugesSubgraph,
-      networkConfig.addresses.contracts.multicall,
-      networkConfig.addresses.contracts.gaugeController,
-      sor.provider
-    ),
-    feeDistributor: new FeeDistributorRepository(
-      networkConfig.addresses.contracts.multicall,
-      networkConfig.addresses.contracts.feeDistributor,
-      networkConfig.addresses.tokens.bal,
-      networkConfig.addresses.tokens.veBal,
-      networkConfig.addresses.tokens.bbaUsd,
-      sor.provider
-    ),
-    feeCollector: new FeeCollectorRepository(
-      networkConfig.addresses.contracts.vault,
-      sor.provider
-    ),
-    tokenYields: new TokenYieldsRepository(),
-  };
-
-  return repositories;
 }
