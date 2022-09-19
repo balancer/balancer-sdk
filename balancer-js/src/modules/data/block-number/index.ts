37b7e80eb611966dd44e1c20aae537e441d226a3
Tim Robinson <tim@timjrobinson.com>
2022-09-19 12:01:59 +0200

    Fix merge issues

--- balancer-js/src/modules/data/block-number/index.ts
+++ balancer-js/src/modules/data/block-number/index.ts
@@ -1,7 +1,8 @@
 import { Findable } from '../types';
+import axios from 'axios';
 
 const query = (timestamp: string) => `{
-  blocks(first: 1, orderBy: number, orderDirection: asc, where: { timestamp_gt: ${timestamp} }) {
+  blocks(first: 1, orderBy: timestamp, orderDirection: asc, where: { timestamp_gt: ${timestamp} }) {
     number
   }
 }`;
@@ -20,32 +21,36 @@ const fetchBlockByTime = async (
   endpoint: string,
   timestamp: string
 ): Promise<number> => {
+  console.time(`fetching blocks ${timestamp}`);
   const payload = {
     query: query(timestamp),
   };
 
-  const response = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  });
+  const response = await axios.post(endpoint, payload);
+  console.timeEnd(`fetching blocks ${timestamp}`);
 
   const {
     data: { blocks },
-  } = (await response.json()) as BlockNumberResponse;
+  } = response.data as BlockNumberResponse;
 
   return parseInt(blocks[0].number);
 };
 
 export class BlockNumberRepository implements Findable<number> {
+  blocks: { [ts: string]: Promise<number> } = {};
+
   constructor(private endpoint: string) {}
 
   async find(from: string): Promise<number | undefined> {
     if (from == 'dayAgo') {
       const dayAgo = `${Math.floor(Date.now() / 1000) - 86400}`;
-      return fetchBlockByTime(this.endpoint, dayAgo);
+      if (!this.blocks[dayAgo]) {
+        this.blocks = {
+          ...this.blocks,
+          [dayAgo]: fetchBlockByTime(this.endpoint, dayAgo),
+        };
+      }
+      return this.blocks[dayAgo];
     }
   }
 
