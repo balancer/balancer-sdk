9bf3290f819ec536817063988b071437d1f7029a
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-10-24 11:20:02 -0300

    Minor fixes and cleanup

--- balancer-js/src/lib/utils/tenderlyHelper.ts
+++ balancer-js/src/lib/utils/tenderlyHelper.ts
@@ -6,6 +6,16 @@ import { networkAddresses } from '@/lib/constants/config';
 dotenv.config();
 const { TENDERLY_USER, TENDERLY_PROJECT, TENDERLY_ACCESS_KEY } = process.env;
 
+const opts = {
+  headers: {
+    'X-Access-Key': TENDERLY_ACCESS_KEY as string,
+  },
+};
+
+type StateOverrides = {
+  [address: string]: { value: { [key: string]: string } };
+};
+
 export const simulateTransaction = async (
   userAddress: string,
   encodedCallData: string,
@@ -16,104 +26,77 @@ export const simulateTransaction = async (
   const relayerAddress = contracts.relayer as string;
   const vaultAddress = contracts.vault as string;
 
-  let staticResult = '';
-  try {
-    let stateOverrides: {
-      [address: string]: { value: { [key: string]: string } };
-    } = {};
-    tokensIn.forEach(
-      (tokenIn) =>
-        (stateOverrides = {
-          ...stateOverrides,
-          [`${tokenIn}`]: {
-            value: {
-              [`_balances[${userAddress}]`]: MaxUint256.toString(),
-              [`_allowances[${userAddress}][${vaultAddress}]`]:
-                MaxUint256.toString(),
-            },
+  // Encode token balances and allowances overrides to max value before performing simulation
+  const encodedStateOverrides = await encodeBalanceAndAllowanceOverrides(
+    userAddress,
+    tokensIn,
+    chainId,
+    vaultAddress
+  );
+
+  // Map encoded-state response into simulate request body by replacing property names
+  const state_objects = Object.fromEntries(
+    Object.keys(encodedStateOverrides).map((address) => {
+      // Object.fromEntries require format [key, value] instead of {key: value}
+      return [address, { storage: encodedStateOverrides[address].value }];
+    })
+  );
+
+  const body = {
+    // -- Standard TX fields --
+    network_id: chainId.toString(),
+    from: userAddress,
+    to: relayerAddress,
+    input: encodedCallData,
+    // gas: 8000000,
+    // gas_price: '0',
+    // value: '0',
+    // -- Simulation config (tenderly specific) --
+    save_if_fails: true,
+    // save: true,
+    state_objects,
+  };
+
+  const SIMULATE_URL = `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/simulate`;
+
+  const resp = await axios.post(SIMULATE_URL, body, opts);
+
+  const simulatedTransactionOutput =
+    resp.data.transaction.transaction_info.call_trace.output;
+
+  return simulatedTransactionOutput;
+};
+
+const encodeBalanceAndAllowanceOverrides = async (
+  userAddress: string,
+  tokens: string[],
+  chainId: number,
+  vaultAddress: string
+): Promise<StateOverrides> => {
+  // Create balances and allowances overrides for each token address provided
+  let stateOverrides: StateOverrides = {};
+  tokens.forEach(
+    (token) =>
+      (stateOverrides = {
+        ...stateOverrides,
+        [`${token}`]: {
+          value: {
+            [`_balances[${userAddress}]`]: MaxUint256.toString(),
+            [`_allowances[${userAddress}][${vaultAddress}]`]:
+              MaxUint256.toString(),
           },
-        })
-    );
-
-    console.log(stateOverrides);
-
-    const ENCODE_STATES_URL = `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/contracts/encode-states`;
-    const encodedStatesBody = {
-      networkID: chainId.toString(),
-      stateOverrides,
-    };
-    // const oda = {
-    //   networkID: chainId.toString(),
-    //   stateOverrides: {
-    //     '0x14468fd5e1de5a5a4882fa5f4e2217c5a8ddcadb': {
-    //       value: {
-    //         '_balances[0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266]':
-    //           '115792089237316195423570985008687907853269984665640564039457584007913129639935',
-    //         '_allowances[0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266][0xBA12222222228d8Ba445958a75a0704d566BF2C8]':
-    //           '115792089237316195423570985008687907853269984665640564039457584007913129639935',
-    //       },
-    //     },
-    //     '0x398106564948feeb1fedea0709ae7d969d62a391': {
-    //       value: {
-    //         '_balances[0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266]':
-    //           '115792089237316195423570985008687907853269984665640564039457584007913129639935',
-    //         '_allowances[0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266][0xBA12222222228d8Ba445958a75a0704d566BF2C8]':
-    //           '115792089237316195423570985008687907853269984665640564039457584007913129639935',
-    //       },
-    //     },
-    //   },
-    // };
-    // console.log(oda);
-
-    const opts = {
-      headers: {
-        'X-Access-Key': TENDERLY_ACCESS_KEY as string,
-      },
-    };
-
-    const encodedStatesResponse = await axios.post(
-      ENCODE_STATES_URL,
-      encodedStatesBody,
-      opts
-    );
-    const encodedStateOverrides = encodedStatesResponse.data.stateOverrides;
-
-    console.log(encodedStateOverrides);
-
-    // Map encoded-state response into simulate request body by replacing property names
-    const state_objects = Object.fromEntries(
-      Object.keys(encodedStateOverrides).map((address) => {
-        // Object.fromEntries require format [key, value] instead of {key: value}
-        return [address, { storage: encodedStateOverrides[address].value }];
+        },
       })
-    );
+  );
 
-    console.log(state_objects);
+  const ENCODE_STATES_URL = `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/contracts/encode-states`;
+  const body = {
+    networkID: chainId.toString(),
+    stateOverrides,
+  };
 
-    const body = {
-      // standard TX fields
-      network_id: chainId.toString(),
-      from: userAddress,
-      to: relayerAddress,
-      input: encodedCallData,
-      // gas: 8000000,
-      // gas_price: '0',
-      // value: '0',
-      // simulation config (tenderly specific)
-      save_if_fails: true,
-      // save: true,
-    };
-
-    const simulateBody = { ...body, state_objects };
-
-    const SIMULATE_URL = `https://api.tenderly.co/api/v1/account/${TENDERLY_USER}/project/${TENDERLY_PROJECT}/simulate`;
-
-    const resp = await axios.post(SIMULATE_URL, simulateBody, opts);
-
-    staticResult = resp.data.transaction.transaction_info.call_trace.output;
-  } catch (error) {
-    const errorMessage = (error as Error).message;
-    throw new Error(errorMessage);
-  }
-  return staticResult;
+  const encodedStatesResponse = await axios.post(ENCODE_STATES_URL, body, opts);
+  const encodedStateOverrides = encodedStatesResponse.data
+    .stateOverrides as StateOverrides;
+  return encodedStateOverrides;
 };
