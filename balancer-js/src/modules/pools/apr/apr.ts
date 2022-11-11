e335093c6a0a2d2bf3685a3984c4c3e3645e5a70
Bruno Eidam Guerios <brunoguerios@gmail.com>
2022-11-11 14:45:37 -0300

    Make protocolYieldFeeCache optional

--- balancer-js/src/modules/pools/apr/apr.ts
+++ balancer-js/src/modules/pools/apr/apr.ts
@@ -126,6 +126,8 @@ export class PoolApr {
           if (token.isExemptFromYieldProtocolFee) {
             apr = tokenYield;
           } else {
+            if (!pool.protocolYieldFeeCache)
+              throw new Error('Missing protocolYieldFeeCache');
             apr = tokenYield * (1 - parseFloat(pool.protocolYieldFeeCache));
           }
         } else {
