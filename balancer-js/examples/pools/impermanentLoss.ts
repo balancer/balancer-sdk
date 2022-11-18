606629eb3e1e92a34c6ce343cf5faa57e8e93690
Skytale Development <dev@skytale.finance>
2022-11-18 08:58:56 +0100

    test: add new data

--- balancer-js/examples/pools/impermanentLoss.ts
+++ balancer-js/examples/pools/impermanentLoss.ts
@@ -25,7 +25,7 @@ const main = async (): Promise<void> => {
   await impermanentLoss('0x000000000088e0120f9e6652cc058aec07564f69', '0x5c6ee304399dbdb9c8ef030ab642b10820db8f56000200000000000000000014');
   await impermanentLoss('0x07dd13b2705050b2f5c60da9f7f0f37b7395945a', '0xc45d42f801105e861e86658648e3678ad7aa70f900010000000000000000011e');
   await impermanentLoss('0x00bcfc8f7471b2e4d21af417dea393c1578c67c1', '0x1e19cf2d73a72ef1332c882f20534b6519be0276000200000000000000000112');
-  // await impermanentLoss('0x0035fc5208ef989c28d47e552e92b0c507d2b318');
+  // await impermanentLoss('0x356226e2f6d49749fd5f0fa5656acf86b20f3485', '0xa13a9247ea42d743238089903570127dda72fe4400000000000000000000035d');
 }
 
 const impermanentLoss = async (userAddress: string, poolId: string): Promise<void> => {
