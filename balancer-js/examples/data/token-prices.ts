914c5749a7f470667f6422f19cef2902e9c96195
bronco <rg3mvdb0@protonmail.com>
2022-11-30 11:57:06 +0100

    fix: coingecko native asset data format

--- balancer-js/examples/data/token-prices.ts
+++ balancer-js/examples/data/token-prices.ts
@@ -8,10 +8,11 @@ const sdk = new BalancerSDK({ network: 1, rpcUrl: '' });
 const { data } = sdk;
 const dai = '0x6b175474e89094c44da98b954eedeac495271d0f';
 const eth = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+const matic = '0x0000000000000000000000000000000000001010';
 
 (async () => {
   // It will be just one request to coingecko
-  const ps = [dai, eth, dai, eth, dai, eth].map((t) => data.tokenPrices.find(t));
+  const ps = [dai, eth, dai, eth, dai, eth, matic].map((t) => data.tokenPrices.find(t));
   const price = await Promise.all(ps);
 
   console.log(price);
