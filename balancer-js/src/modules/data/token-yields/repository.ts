7d63a9271a83ad381ba269d0012d1f0f825969cc
bronco <rg3mvdb0@protonmail.com>
2022-12-14 16:49:29 +0100

    new: maticx yield

--- balancer-js/src/modules/data/token-yields/repository.ts
+++ balancer-js/src/modules/data/token-yields/repository.ts
@@ -10,6 +10,7 @@ import {
 import { aave, allYieldTokens as aaveTokens } from './tokens/aave';
 import { overnight, yieldTokens as overnightTokens } from './tokens/overnight';
 import { sfrxETH, yieldTokens as fraxTokens } from './tokens/sfrxeth';
+import { maticX, yieldTokens as staderLabsTokens } from './tokens/maticx';
 import { Network, Findable } from '@/types';
 
 /**
@@ -28,6 +29,7 @@ const yieldSourceMap: { [address: string]: AprFetcher } = Object.fromEntries([
   ...Object.values(overnightTokens).map((k) => [k, overnight]),
   ...Object.values(rocketpoolTokens).map((k) => [k, rocketpool]),
   ...Object.values(fraxTokens).map((k) => [k, sfrxETH]),
+  ...Object.values(staderLabsTokens).map((k) => [k, maticX]),
 ]);
 
 export class TokenYieldsRepository implements Findable<number> {
