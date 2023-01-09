38bfec89a101117d6ecdebb4d48786d9cf0d2228
Luiz Gustavo Abou Hatem de Liz <luizgustavoahsc@gmail.com>
2023-01-09 18:39:07 -0300

    Removing unused pool from test pools archive;
    Finished composable pool testing on mainnet;

--- balancer-js/src/modules/pools/factory/composable-stable/composable-stable.factory.spec.ts
+++ balancer-js/src/modules/pools/factory/composable-stable/composable-stable.factory.spec.ts
@@ -1,10 +1,6 @@
 // yarn test:only ./src/modules/pools/factory/composable-stable/composable-stable.factory.spec.ts
 import { expect } from 'chai';
-import {
-  JsonRpcProvider,
-  Log,
-  TransactionReceipt,
-} from '@ethersproject/providers';
+import { Log, TransactionReceipt } from '@ethersproject/providers';
 import { ethers } from 'hardhat';
 import { Network, PoolType } from '@/types';
 import { ADDRESSES } from '@/test/lib/constants';
@@ -12,12 +8,20 @@ import { AddressZero } from '@ethersproject/constants';
 import { BalancerSDK } from '@/modules/sdk.module';
 import { Interface, LogDescription } from '@ethersproject/abi';
 import composableStableFactoryAbi from '@/lib/abi/ComposableStableFactory.json';
+import { forkSetup } from '@/test/lib/utils';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // const network = Network.GOERLI;
 // const rpcUrl = 'http://localhost:8000';
+// const alchemyRpcUrl = `${process.env.ALCHEMY_URL_GOERLI}`;
+// const blockNumber = 8200000;
 
 const network = Network.MAINNET;
-const rpcUrl = 'http://localhost:8545/';
+const rpcUrl = 'http://localhost:8545';
+const alchemyRpcUrl = `${process.env.ALCHEMY_URL}`;
+const blockNumber = 16320000;
 
 const name = 'My-Test-Pool-Name';
 const symbol = 'My-Test-Pool-Symbol';
@@ -38,67 +42,64 @@ const amplificationParameter = '2';
 const swapFee = '0.01';
 
 describe('creating composable stable pool', async () => {
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl, network);
+  const signer = provider.getSigner();
+  const sdkConfig = {
+    network,
+    rpcUrl,
+  };
+  const balancer = new BalancerSDK(sdkConfig);
+  const composableStablePoolFactory = balancer.pools.poolFactory.of(
+    PoolType.ComposableStable
+  );
   context('create', async () => {
+    beforeEach(async () => {
+      await forkSetup(signer, [], [], [], alchemyRpcUrl, blockNumber, false);
+    });
     it('should create a pool', async () => {
-      try {
-        const provider = new ethers.providers.JsonRpcProvider(rpcUrl, network);
-        const signer = provider.getSigner();
-        const sdkConfig = {
-          network,
-          rpcUrl,
-        };
-        const balancer = new BalancerSDK(sdkConfig);
-        const composableStablePoolFactory = balancer.pools.poolFactory.of(
-          PoolType.ComposableStable
-        );
-        const { to, data } = composableStablePoolFactory.create({
-          contractAddress,
-          name,
-          symbol,
-          tokenAddresses,
-          amplificationParameter,
-          rateProviders,
-          tokenRateCacheDurations,
-          exemptFromYieldProtocolFeeFlags,
-          swapFee,
-          owner,
-        });
-        const signerAddress = await signer.getAddress();
-        const tx = await signer.sendTransaction({
-          from: signerAddress,
-          to,
-          data,
-          gasLimit: 6721975,
-        });
-        const receipt: TransactionReceipt =
-          await provider.getTransactionReceipt(tx.hash);
+      const { to, data } = composableStablePoolFactory.create({
+        contractAddress,
+        name,
+        symbol,
+        tokenAddresses,
+        amplificationParameter,
+        rateProviders,
+        tokenRateCacheDurations,
+        exemptFromYieldProtocolFeeFlags,
+        swapFee,
+        owner,
+      });
+      const signerAddress = await signer.getAddress();
+      const tx = await signer.sendTransaction({
+        from: signerAddress,
+        to,
+        data,
+        gasLimit: 30000000,
+      });
+      await tx.wait();
+      const receipt: TransactionReceipt = await provider.getTransactionReceipt(
+        tx.hash
+      );
 
-        const composableStableFactoryInterface = new Interface(
-          composableStableFactoryAbi
-        );
+      console.log(receipt);
+      const composableStableFactoryInterface = new Interface(
+        composableStableFactoryAbi
+      );
 
-        const poolCreationEvent: LogDescription | null | undefined =
-          receipt.logs
-            .filter((log: Log) => {
-              return (
-                log.address.toUpperCase() === contractAddress.toUpperCase()
-              );
-            })
-            .map((log) => {
-              try {
-                return composableStableFactoryInterface.parseLog(log);
-              } catch (error) {
-                console.error(error);
-                return null;
-              }
-            })
-            .find((parsedLog) => parsedLog?.name === 'PoolCreated');
-        console.log(!!poolCreationEvent);
-        expect(!!poolCreationEvent).to.be.true;
-      } catch (error) {
-        console.error(error);
-        throw error;
-      }
+      const poolCreationEvent: LogDescription | null | undefined = receipt.logs
+        .filter((log: Log) => {
+          return log.address.toUpperCase() === contractAddress.toUpperCase();
+        })
+        .map((log) => {
+          try {
+            return composableStableFactoryInterface.parseLog(log);
+          } catch (error) {
+            console.error(error);
+            return null;
+          }
+        })
+        .find((parsedLog) => parsedLog?.name === 'PoolCreated');
+      expect(!!poolCreationEvent).to.be.true;
     });
   });
 });
