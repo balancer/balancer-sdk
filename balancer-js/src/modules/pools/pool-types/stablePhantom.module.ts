4420d4a361303893bc2fb89680ecde78e00fae95
Tim Robinson <tim@timjrobinson.com>
2023-02-02 19:40:36 +1000

    Use Stable liquidity calculations for StablePhantom pools
    - StablePhantom pools can contain ERC20 tokens so use the stable math
      for calculating their liquidity instead of erroring

--- balancer-js/src/modules/pools/pool-types/stablePhantom.module.ts
+++ balancer-js/src/modules/pools/pool-types/stablePhantom.module.ts
@@ -1,6 +1,6 @@
 import { StablePhantomPoolExit } from './concerns/stablePhantom/exit.concern';
 import { StablePhantomPoolJoin } from './concerns/stablePhantom/join.concern';
-import { StablePhantomPoolLiquidity } from './concerns/stablePhantom/liquidity.concern';
+import { StablePoolLiquidity } from './concerns/stable/liquidity.concern';
 import { PhantomStablePoolSpotPrice } from './concerns/stablePhantom/spotPrice.concern';
 import { StablePhantomPriceImpact } from './concerns/stablePhantom/priceImpact.concern';
 import { PoolType } from './pool-type.interface';
@@ -16,7 +16,7 @@ export class StablePhantom implements PoolType {
   constructor(
     public exit: ExitConcern = new StablePhantomPoolExit(),
     public join: JoinConcern = new StablePhantomPoolJoin(),
-    public liquidity: LiquidityConcern = new StablePhantomPoolLiquidity(),
+    public liquidity: LiquidityConcern = new StablePoolLiquidity(),
     public spotPriceCalculator: SpotPriceConcern = new PhantomStablePoolSpotPrice(),
     public priceImpactCalculator: PriceImpactConcern = new StablePhantomPriceImpact()
   ) {}
