5604935b5f4352e07b84c5546374f9bee43d25eb
Luiz Gustavo Abou Hatem de Liz <luizgustavoahsc@gmail.com>
2023-02-14 12:55:33 -0300

    Changing rpcUrl to localhost

--- balancer-js/src/modules/pools/pool-types/concerns/stable/exit.concern.spec.ts
+++ balancer-js/src/modules/pools/pool-types/concerns/stable/exit.concern.spec.ts
@@ -5,7 +5,7 @@ import { Network, PoolWithMethods } from '@/.';
 import { TestPoolHelper } from '@/test/lib/utils';
 import { AddressZero } from '@ethersproject/constants';
 
-const rpcUrl = '';
+const rpcUrl = 'http://127.0.0.1:8545';
 const network = Network.MAINNET;
 // This blockNumber is before protocol fees were switched on (Oct `21), for blockNos after this tests will fail because results don't 100% match
 const blockNumber = 13309758;
