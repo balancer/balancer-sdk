d5d44ea55148e7d2f25ec9dafed70bdf498ee925
Luiz Gustavo Abou Hatem de Liz <luizgustavoahsc@gmail.com>
2023-03-22 19:35:56 -0300

    Refactoring weighted pool tests;

--- balancer-js/src/modules/pools/factory/weighted/weighted.factory.integration.spec.ts
+++ balancer-js/src/modules/pools/factory/weighted/weighted.factory.integration.spec.ts
@@ -1,131 +1,157 @@
 // yarn test:only ./src/modules/pools/factory/weighted/weighted.factory.integration.spec.ts
-import { Interface, LogDescription } from '@ethersproject/abi';
 import { parseFixed } from '@ethersproject/bignumber';
 import { expect } from 'chai';
 import dotenv from 'dotenv';
-import { ethers } from 'hardhat';
-
-import { Vault__factory } from '@/contracts/factories/Vault__factory';
-import { BalancerSDK } from '@/modules/sdk.module';
-import { ADDRESSES } from '@/test/lib/constants';
-import { forkSetup, sendTransactionGetBalances } from '@/test/lib/utils';
-import { Network, PoolType } from '@/types';
-import { findEventInReceiptLogs } from '@/lib/utils';
 
 dotenv.config();
 
+import { BalancerSDK } from '@/modules/sdk.module';
+import { ADDRESSES } from '@/test/lib/constants';
+import {
+  forkSetup,
+  getBalances,
+  sendTransactionGetBalances,
+} from '@/test/lib/utils';
+import { Network, PoolType } from '@/types';
+import { JsonRpcProvider, TransactionReceipt } from '@ethersproject/providers';
+import { WeightedCreatePoolParameters } from '@/modules/pools/factory/types';
+import { WeightedPool, WeightedPool__factory } from '@/contracts';
+import { WeightedMaths } from '@balancer-labs/sor';
+import { SolidityMaths } from '@/lib/utils/solidityMaths';
+
 const network = Network.MAINNET;
 const rpcUrl = 'http://127.0.0.1:8545';
-const alchemyRpcUrl = `${process.env.ALCHEMY_URL}`;
-const blockNumber = 16720000;
-
-const name = 'My-Test-Pool-Name';
-const symbol = 'My-Test-Pool-Symbol';
-
+const balancer = new BalancerSDK({
+  network,
+  rpcUrl,
+});
+const provider = new JsonRpcProvider(rpcUrl, network);
+const signer = provider.getSigner();
 const addresses = ADDRESSES[network];
 
-const USDC_address = addresses.USDC.address;
-const USDT_address = addresses.USDT.address;
-
-const owner = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
-const tokenAddresses = [USDC_address, USDT_address];
-const swapFeeEvm = `${1e16}`;
-const weights = [`${0.2e18}`, `${0.8e18}`];
-const slots = [addresses.USDC.slot, addresses.USDT.slot];
-const balances = [
-  parseFixed('100000', 6).toString(),
-  parseFixed('100000', 6).toString(),
-];
 describe('creating weighted pool', () => {
-  const provider = new ethers.providers.JsonRpcProvider(rpcUrl, network);
-  const signer = provider.getSigner();
-  const sdkConfig = {
-    network,
-    rpcUrl,
-  };
-  const balancer = new BalancerSDK(sdkConfig);
+  const poolTokens = [addresses.USDC, addresses.USDT];
+  const rawAmount = '1000000000';
+  const amountsIn = poolTokens.map((p) =>
+    parseFixed(rawAmount, p.decimals).toString()
+  );
+  const amountsInEvm = poolTokens.map(() =>
+    parseFixed(rawAmount, 18).toString()
+  );
   const weightedPoolFactory = balancer.pools.poolFactory.of(PoolType.Weighted);
-  let poolAddress = '';
-  let poolId = '';
-
+  let poolAddress: string;
+  let poolId: string;
+  let poolParams: WeightedCreatePoolParameters;
+  let signerAddress: string;
+  let pool: WeightedPool;
   context('create and init join', async () => {
     before(async () => {
+      signerAddress = await signer.getAddress();
       await forkSetup(
         signer,
-        tokenAddresses,
-        slots,
-        balances,
-        alchemyRpcUrl,
-        blockNumber,
+        poolTokens.map((p) => p.address),
+        poolTokens.map((p) => p.slot),
+        amountsIn,
+        `${process.env.ALCHEMY_URL}`,
+        16720000,
         false
       );
+      poolParams = {
+        name: 'My-Test-Pool-Name',
+        symbol: 'My-Test-Pool-Symbol',
+        tokenAddresses: poolTokens.map((p) => p.address),
+        weights: [
+          parseFixed('0.2', 18).toString(),
+          parseFixed('0.8', 18).toString(),
+        ],
+        swapFeeEvm: parseFixed('0.01', 18).toString(),
+        owner: signerAddress,
+      };
     });
-    it('should create a pool', async () => {
-      const signerAddress = await signer.getAddress();
-      const { to, data } = weightedPoolFactory.create({
-        name,
-        symbol,
-        tokenAddresses,
-        weights,
-        swapFeeEvm,
-        owner,
-      });
-      const { transactionReceipt } = await sendTransactionGetBalances(
-        [],
-        signer,
-        signerAddress,
-        to as string,
-        data as string
-      );
+    context('pool creation', async () => {
+      let transactionReceipt: TransactionReceipt;
 
-      const poolInfo = await weightedPoolFactory.getPoolAddressAndIdWithReceipt(
-        signer.provider,
-        transactionReceipt
-      );
-      poolAddress = poolInfo.poolAddress;
-      poolId = poolInfo.poolId;
-      //Verifying if the address and id are valid
-      expect(poolId.length).to.equal(66);
-      expect(poolAddress.length).to.equal(42);
-      expect(poolId.indexOf('x')).to.equal(1);
-      expect(poolAddress.indexOf('x')).to.equal(1);
-      return;
-    });
-    it('should init join a pool', async () => {
-      const signerAddress = await signer.getAddress();
-      const amountsIn = [
-        parseFixed('2000', 6).toString(),
-        parseFixed('8000', 6).toString(),
-      ];
-      const { to, data } = weightedPoolFactory.buildInitJoin({
-        joiner: signerAddress,
-        poolId,
-        poolAddress,
-        tokensIn: tokenAddresses,
-        amountsIn,
+      it('should send creation tx', async () => {
+        const txInfo = weightedPoolFactory.create(poolParams);
+        transactionReceipt = await (
+          await signer.sendTransaction(txInfo)
+        ).wait();
+        expect(transactionReceipt.status).to.eql(1);
       });
-      const { transactionReceipt, balanceDeltas } =
-        await sendTransactionGetBalances(
-          [...tokenAddresses, poolAddress],
+      it('should have correct pool info on creation', async () => {
+        ({ poolId, poolAddress } =
+          await weightedPoolFactory.getPoolAddressAndIdWithReceipt(
+            provider,
+            transactionReceipt
+          ));
+        pool = WeightedPool__factory.connect(poolAddress, provider);
+        const id = await pool.getPoolId();
+        const owner = await pool.getOwner();
+        const swapFee = await pool.getSwapFeePercentage();
+        const name = await pool.name();
+        const symbol = await pool.symbol();
+        const normalizedWeights = await pool.getNormalizedWeights();
+        expect(id).to.eq(poolId);
+        expect(name).to.eq(poolParams.name);
+        expect(symbol).to.eq(poolParams.symbol);
+        expect(swapFee.toString()).to.eq(poolParams.swapFeeEvm);
+        expect(
+          normalizedWeights.map((weight) => weight.toString())
+        ).to.deep.equal(poolParams.weights);
+        expect(owner).to.eq(poolParams.owner);
+      });
+    });
+    context('init join', async () => {
+      it('should send init join tx', async () => {
+        const { to, data } = weightedPoolFactory.buildInitJoin({
+          joiner: signerAddress,
+          poolId,
+          poolAddress,
+          tokensIn: poolTokens.map((p) => p.address),
+          amountsIn,
+        });
+        const { transactionReceipt } = await sendTransactionGetBalances(
+          [...poolTokens.map((p) => p.address), poolAddress],
           signer,
           signerAddress,
-          to as string,
-          data as string
+          to,
+          data
         );
-      const vaultInterface = new Interface(Vault__factory.abi);
-      const poolInitJoinEvent: LogDescription = findEventInReceiptLogs({
-        receipt: transactionReceipt,
-        to,
-        contractInterface: vaultInterface,
-        logName: 'PoolBalanceChanged',
+        expect(transactionReceipt.status).to.eql(1);
       });
-      expect(!!poolInitJoinEvent).to.be.true;
-      expect(
-        balanceDeltas
-          .map((delta) => delta.toString())
-          .slice(0, amountsIn.length)
-      ).deep.equal(amountsIn);
-      expect(balanceDeltas[amountsIn.length].toBigInt() > BigInt(0)).to.be.true;
+      it('should have joined with full balance', async () => {
+        const poolTokenBalances = await getBalances(
+          poolTokens.map((p) => p.address),
+          signer,
+          signerAddress
+        );
+        poolTokenBalances.forEach((b) => expect(b.isZero()).is.true);
+      });
+      it('should receive correct BPT Amount', async () => {
+        const bptBalance = await getBalances(
+          [poolAddress],
+          signer,
+          signerAddress
+        );
+
+        // The BPT Amount is calculated as:
+        // BPTAmount = invariant * n - 1000000
+        // where n is the number of tokens in the pool
+        const invariantAfterJoin = WeightedMaths._calculateInvariant(
+          poolParams.weights.map((weight) => BigInt(weight.toString())),
+          amountsInEvm.map(BigInt)
+        );
+        const invariantAfterJoinMultipliedByN = SolidityMaths.mul(
+          invariantAfterJoin,
+          BigInt(amountsInEvm.length)
+        );
+        //The pool sends 1000000 BPT to the address zero, so the BPT Supply can never be drained
+        const expectedBptBalance = SolidityMaths.sub(
+          invariantAfterJoinMultipliedByN,
+          BigInt(1e6)
+        );
+        expect(bptBalance.toString()).to.eq(expectedBptBalance.toString());
+      });
     });
   });
 });
