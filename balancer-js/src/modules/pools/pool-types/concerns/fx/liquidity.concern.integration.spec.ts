dd387c4975cb5e2cac4335b52ea8bd45145aa932
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2023-05-26 14:38:21 -0300

    Fix fx liquidity integration tests on polygon

--- balancer-js/src/modules/pools/pool-types/concerns/fx/liquidity.concern.integration.spec.ts
+++ balancer-js/src/modules/pools/pool-types/concerns/fx/liquidity.concern.integration.spec.ts
@@ -1,22 +1,31 @@
 // yarn test:only ./src/modules/pools/pool-types/concerns/fx/liquidity.concern.integration.spec.ts
-import dotenv from 'dotenv';
-import { Network, PoolWithMethods } from '@/types';
-import { forkSetup, TestPoolHelper } from '@/test/lib/utils';
-import { ethers } from 'hardhat';
-import { BalancerSDK } from '@/modules/sdk.module';
-import { FXPool__factory } from '@/contracts';
-import { Contract } from '@ethersproject/contracts';
 import { expect } from 'chai';
+import dotenv from 'dotenv';
 import { formatFixed, parseFixed } from '@ethersproject/bignumber';
+import { Contract } from '@ethersproject/contracts';
+import { JsonRpcProvider } from '@ethersproject/providers';
+
+import { FXPool__factory } from '@/contracts';
+import { BALANCER_NETWORK_CONFIG } from '@/lib/constants/config';
 import { SolidityMaths } from '@/lib/utils/solidityMaths';
+import { Pools } from '@/modules/pools';
+import { BalancerSDK } from '@/modules/sdk.module';
+import {
+  FORK_NODES,
+  forkSetup,
+  getPoolFromFile,
+  RPC_URLS,
+  updateFromChain,
+} from '@/test/lib/utils';
+import { Network, PoolWithMethods } from '@/types';
 
 dotenv.config();
 
 const network = Network.POLYGON;
-const { ALCHEMY_URL_POLYGON: rpcUrlArchive } = process.env;
-const rpcUrlLocal = 'http://127.0.0.1:8137';
+const rpcUrlRemote = FORK_NODES[network];
+const rpcUrlLocal = RPC_URLS[network];
 
-const provider = new ethers.providers.JsonRpcProvider(rpcUrlLocal, network);
+const provider = new JsonRpcProvider(rpcUrlLocal, network);
 const signer = provider.getSigner();
 const testPoolId =
   '0x726e324c29a1e49309672b244bdc4ff62a270407000200000000000000000702';
@@ -29,23 +38,19 @@ describe('FX Pool - Calculate Liquidity', () => {
     rpcUrl: rpcUrlLocal,
   };
   const balancer = new BalancerSDK(sdkConfig);
+
   before(async () => {
-    const testPool = new TestPoolHelper(
-      testPoolId,
-      network,
-      rpcUrlLocal,
-      blockNumber
-    );
-    // Gets initial pool info from Subgraph
-    pool = await testPool.getPool();
+    let testPool = await getPoolFromFile(testPoolId, network);
+    testPool = await updateFromChain(testPool, network, provider);
 
     // Setup forked network, set initial token balances and allowances
-    await forkSetup(signer, [], [], [], rpcUrlArchive as string, undefined);
+    await forkSetup(signer, [], [], [], rpcUrlRemote as string, blockNumber);
 
     // Update pool info with onchain state from fork block no
-    pool = await testPool.getPool();
+    pool = Pools.wrap(testPool, BALANCER_NETWORK_CONFIG[network]);
   });
-  it('calculating liquidity', async () => {
+
+  it('should match liquidity from contract with 5% of margin error', async () => {
     const liquidity = await balancer.pools.liquidity(pool);
     const poolInterface = FXPool__factory.createInterface();
     const poolContract = new Contract(pool.address, poolInterface, provider);
