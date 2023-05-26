cc84e49b994d883d689efe652451396465344a59
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2023-05-26 15:19:49 -0300

    Update liquidity integration tests (wip)

--- balancer-js/src/modules/pools/pool-types/concerns/fx/liquidity.concern.integration.spec.ts
+++ balancer-js/src/modules/pools/pool-types/concerns/fx/liquidity.concern.integration.spec.ts
@@ -2,13 +2,10 @@
 import { expect } from 'chai';
 import dotenv from 'dotenv';
 import { formatFixed, parseFixed } from '@ethersproject/bignumber';
-import { Contract } from '@ethersproject/contracts';
 import { JsonRpcProvider } from '@ethersproject/providers';
 
 import { FXPool__factory } from '@/contracts';
-import { BALANCER_NETWORK_CONFIG } from '@/lib/constants/config';
 import { SolidityMaths } from '@/lib/utils/solidityMaths';
-import { Pools } from '@/modules/pools';
 import { BalancerSDK } from '@/modules/sdk.module';
 import {
   FORK_NODES,
@@ -17,7 +14,7 @@ import {
   RPC_URLS,
   updateFromChain,
 } from '@/test/lib/utils';
-import { Network, PoolWithMethods } from '@/types';
+import { Network, Pool } from '@/types';
 
 dotenv.config();
 
@@ -29,7 +26,7 @@ const provider = new JsonRpcProvider(rpcUrlLocal, network);
 const signer = provider.getSigner();
 const testPoolId =
   '0x726e324c29a1e49309672b244bdc4ff62a270407000200000000000000000702';
-let pool: PoolWithMethods;
+let pool: Pool;
 const blockNumber = 43015527;
 
 describe('FX Pool - Calculate Liquidity', () => {
@@ -40,24 +37,25 @@ describe('FX Pool - Calculate Liquidity', () => {
   const balancer = new BalancerSDK(sdkConfig);
 
   before(async () => {
-    let testPool = await getPoolFromFile(testPoolId, network);
-    testPool = await updateFromChain(testPool, network, provider);
+    pool = await getPoolFromFile(testPoolId, network);
 
     // Setup forked network, set initial token balances and allowances
     await forkSetup(signer, [], [], [], rpcUrlRemote as string, blockNumber);
 
     // Update pool info with onchain state from fork block no
-    pool = Pools.wrap(testPool, BALANCER_NETWORK_CONFIG[network]);
+    pool = await updateFromChain(pool, network, provider);
   });
 
   it('should match liquidity from contract with 5% of margin error', async () => {
     const liquidity = await balancer.pools.liquidity(pool);
-    const poolInterface = FXPool__factory.createInterface();
-    const poolContract = new Contract(pool.address, poolInterface, provider);
+    const poolContract = FXPool__factory.connect(pool.address, provider);
     const liquidityFromContract = (
       await poolContract.liquidity()
     ).total_.toBigInt();
     const liquidityBigInt = parseFixed(liquidity, 18).toBigInt();
+    console.log('liquidityBigInt      ', liquidityBigInt);
+    console.log('liquidityFromContract', liquidityFromContract);
+    console.log('totalLiquidity       ', pool.totalLiquidity);
     // expecting 5% of margin error
     expect(
       parseFloat(
--- balancer-js/src/modules/pools/pool-types/concerns/gyro/liquidity.concern.integration.spec.ts
+++ balancer-js/src/modules/pools/pool-types/concerns/gyro/liquidity.concern.integration.spec.ts
@@ -1,49 +1,53 @@
 // yarn test:only ./src/modules/pools/pool-types/concerns/gyro/liquidity.concern.integration.spec.ts
-import dotenv from 'dotenv';
-import { Network, PoolWithMethods } from '@/types';
-import { forkSetup, TestPoolHelper } from '@/test/lib/utils';
-import { ethers } from 'hardhat';
-import { BalancerSDK } from '@/modules/sdk.module';
 import { expect } from 'chai';
+import dotenv from 'dotenv';
 import { formatFixed, parseFixed } from '@ethersproject/bignumber';
+import { JsonRpcProvider } from '@ethersproject/providers';
+
 import { SolidityMaths } from '@/lib/utils/solidityMaths';
+import { BalancerSDK } from '@/modules/sdk.module';
+import {
+  FORK_NODES,
+  forkSetup,
+  getPoolFromFile,
+  RPC_URLS,
+  updateFromChain,
+} from '@/test/lib/utils';
+import { Network, Pool } from '@/types';
 
 dotenv.config();
 
-const network = Network.POLYGON;
-const { ALCHEMY_URL_POLYGON: rpcUrlArchive } = process.env;
-const rpcUrlLocal = 'http://127.0.0.1:8137';
-
-const provider = new ethers.providers.JsonRpcProvider(rpcUrlLocal, network);
-const signer = provider.getSigner();
-const blockNumber = 43015527;
-
 describe('Gyro Pools - Calculate Liquidity', () => {
+  const network = Network.POLYGON;
+  const rpcUrlRemote = FORK_NODES[network];
+  const rpcUrlLocal = RPC_URLS[network];
+  const provider = new JsonRpcProvider(rpcUrlLocal, network);
+  const signer = provider.getSigner();
+  const blockNumber = 43015527;
   const sdkConfig = {
     network,
     rpcUrl: rpcUrlLocal,
   };
   const balancer = new BalancerSDK(sdkConfig);
+  let testPoolId: string;
+  let pool: Pool;
+
+  beforeEach(async () => {
+    pool = await getPoolFromFile(testPoolId, network);
+
+    // Setup forked network, set initial token balances and allowances
+    await forkSetup(signer, [], [], [], rpcUrlRemote as string, blockNumber);
+
+    // Update pool info with onchain state from fork block no
+    pool = await updateFromChain(pool, network, provider);
+  });
+
   context('GyroE Pools', () => {
-    const testPoolId =
-      '0x97469e6236bd467cd147065f77752b00efadce8a0002000000000000000008c0';
-    let pool: PoolWithMethods;
     before(async () => {
-      const testPool = new TestPoolHelper(
-        testPoolId,
-        network,
-        rpcUrlLocal,
-        blockNumber
-      );
-      // Gets initial pool info from Subgraph
-      pool = await testPool.getPool();
-
-      // Setup forked network, set initial token balances and allowances
-      await forkSetup(signer, [], [], [], rpcUrlArchive as string, undefined);
-
-      // Update pool info with onchain state from fork block no
-      pool = await testPool.getPool();
+      testPoolId =
+        '0x97469e6236bd467cd147065f77752b00efadce8a0002000000000000000008c0';
     });
+
     it('calculating liquidity', async () => {
       const liquidity = await balancer.pools.liquidity(pool);
       const liquidityFromContract = parseFixed(
@@ -51,6 +55,8 @@ describe('Gyro Pools - Calculate Liquidity', () => {
         18
       ).toBigInt();
       const liquidityBigInt = parseFixed(liquidity, 18).toBigInt();
+      console.log('liquidityBigInt      ', liquidityBigInt);
+      console.log('liquidityFromContract', liquidityFromContract);
       // expecting 5% of margin error
       expect(
         parseFloat(
@@ -62,26 +68,13 @@ describe('Gyro Pools - Calculate Liquidity', () => {
       ).to.be.closeTo(1, 0.05);
     });
   });
+
   context('Gyro V2 Pools', () => {
-    const testPoolId =
-      '0xdac42eeb17758daa38caf9a3540c808247527ae3000200000000000000000a2b';
-    let pool: PoolWithMethods;
     before(async () => {
-      const testPool = new TestPoolHelper(
-        testPoolId,
-        network,
-        rpcUrlLocal,
-        blockNumber
-      );
-      // Gets initial pool info from Subgraph
-      pool = await testPool.getPool();
-
-      // Setup forked network, set initial token balances and allowances
-      await forkSetup(signer, [], [], [], rpcUrlArchive as string, undefined);
-
-      // Update pool info with onchain state from fork block no
-      pool = await testPool.getPool();
+      testPoolId =
+        '0xdac42eeb17758daa38caf9a3540c808247527ae3000200000000000000000a2b';
     });
+
     it('calculating liquidity', async () => {
       const liquidity = await balancer.pools.liquidity(pool);
       const liquidityFromContract = parseFixed(
@@ -89,6 +82,9 @@ describe('Gyro Pools - Calculate Liquidity', () => {
         18
       ).toBigInt();
       const liquidityBigInt = parseFixed(liquidity, 18).toBigInt();
+
+      console.log('liquidityBigInt      ', liquidityBigInt);
+      console.log('liquidityFromContract', liquidityFromContract);
       // expecting 5% of margin error
       expect(
         parseFloat(
@@ -100,26 +96,13 @@ describe('Gyro Pools - Calculate Liquidity', () => {
       ).to.be.closeTo(1, 0.05);
     });
   });
+
   context('Gyro V3 Pools', () => {
-    const testPoolId =
-      '0x17f1ef81707811ea15d9ee7c741179bbe2a63887000100000000000000000799';
-    let pool: PoolWithMethods;
     before(async () => {
-      const testPool = new TestPoolHelper(
-        testPoolId,
-        network,
-        rpcUrlLocal,
-        blockNumber
-      );
-      // Gets initial pool info from Subgraph
-      pool = await testPool.getPool();
-
-      // Setup forked network, set initial token balances and allowances
-      await forkSetup(signer, [], [], [], rpcUrlArchive as string, undefined);
-
-      // Update pool info with onchain state from fork block no
-      pool = await testPool.getPool();
+      testPoolId =
+        '0x17f1ef81707811ea15d9ee7c741179bbe2a63887000100000000000000000799';
     });
+
     it('calculating liquidity', async () => {
       const liquidity = await balancer.pools.liquidity(pool);
       const liquidityFromContract = parseFixed(
@@ -127,6 +110,8 @@ describe('Gyro Pools - Calculate Liquidity', () => {
         18
       ).toBigInt();
       const liquidityBigInt = parseFixed(liquidity, 18).toBigInt();
+      console.log('liquidityBigInt      ', liquidityBigInt);
+      console.log('liquidityFromContract', liquidityFromContract);
       // expecting 5% of margin error
       expect(
         parseFloat(
