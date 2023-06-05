adfcc0f32cb279cecd4a2ea2d1434c99537b9c58
Tim Robinson <tim@timjrobinson.com>
2023-06-05 15:19:15 +1000

    Feat: Avalanche Support

--- balancer-js/src/lib/constants/config.ts
+++ balancer-js/src/lib/constants/config.ts
@@ -506,8 +506,6 @@ export const BALANCER_NETWORK_CONFIG: Record<Network, BalancerNetworkConfig> = {
         composableStablePoolFactory:
           '0x8eA89804145c007e7D226001A96955ad53836087',
         erc4626LinearPoolFactory: '0x6B1Da720Be2D11d95177ccFc40A917c2688f396c',
-        feeDistributor: '',
-        gaugeController: '',
         gearboxLinearPoolFactory: '0x687b8C9b41E01Be8B591725fac5d5f52D0564d79',
         multicall: '0xcA11bde05977b3631167028862bE2a173976CA11',
         protocolFeePercentagesProvider:
@@ -515,7 +513,6 @@ export const BALANCER_NETWORK_CONFIG: Record<Network, BalancerNetworkConfig> = {
         relayer: '0x4678731DC41142A902a114aC5B2F77b63f4a259D',
         vault: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
         weightedPoolFactory: '0x03F3Fb107e74F2EAC9358862E91ad3c692712054',
-        yearnLinearPoolFactory: '',
       },
       tokens: {
         bal: '0x120eF59b80774F02211563834d8E3b72cb1649d6',
@@ -530,6 +527,42 @@ export const BALANCER_NETWORK_CONFIG: Record<Network, BalancerNetworkConfig> = {
       coingecko: {
         nativeAssetId: 'eth',
         platformId: 'polygon-zkevm',
+      },
+    },
+    pools: {},
+    poolsToIgnore: [],
+    sorConnectingTokens: [],
+  },
+  [Network.AVALANCHE]: {
+    chainId: Network.AVALANCHE, //43114
+    addresses: {
+      contracts: {
+        aaveLinearPoolFactory: '0x6caf662b573F577DE01165d2d38D1910bba41F8A',
+        balancerHelpers: '0x8E9aa87E45e92bad84D5F8DD1bff34Fb92637dE9',
+        balancerMinterAddress: '0xEa924b45a3fcDAAdf4E5cFB1665823B8F8F2039B',
+        composableStablePoolFactory:
+          '0x3B1eb8EB7b43882b385aB30533D9A2BeF9052a98',
+        erc4626LinearPoolFactory: '0x4507d91Cd2C0D51D9B4F30Bf0B93AFC938A70BA5',
+        multicall: '0xcA11bde05977b3631167028862bE2a173976CA11',
+        protocolFeePercentagesProvider:
+          '0x239e55F427D44C3cc793f49bFB507ebe76638a2b',
+        relayer: '0x03F1ab8b19bcE21EB06C364aEc9e40322572a1e9',
+        vault: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+        weightedPoolFactory: '0x230a59F4d9ADc147480f03B0D3fFfeCd56c3289a',
+      },
+      tokens: {
+        bal: '0x8239A6b877804206C7799028232A7188DA487CeC',
+        wrappedNativeAsset: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+      },
+    },
+    urls: {
+      subgraph:
+        'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-avalanche-v2',
+    },
+    thirdParty: {
+      coingecko: {
+        nativeAssetId: 'avalanche-2',
+        platformId: 'avalanche',
       },
     },
     pools: {},
