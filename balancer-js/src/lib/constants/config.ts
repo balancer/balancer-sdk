0834d9afebf955ee5ab5cd498911aeadd5bd265b
bronco <rg3mvdb0@protonmail.com>
2023-07-24 22:03:27 +0200

    adding subgraph urls on optimism

--- balancer-js/src/lib/constants/config.ts
+++ balancer-js/src/lib/constants/config.ts
@@ -241,7 +241,10 @@ export const BALANCER_NETWORK_CONFIG: Record<Network, BalancerNetworkConfig> = {
     urls: {
       subgraph:
         'https://api.thegraph.com/subgraphs/name/beethovenxfi/beethovenx-optimism',
-      gaugesSubgraph: '',
+      gaugesSubgraph:
+        'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gauges-optimism',
+      blockNumberSubgraph:
+        'https://api.thegraph.com/subgraphs/name/lyra-finance/optimism-mainnet-blocks',
     },
     pools: {},
     sorConnectingTokens: [
