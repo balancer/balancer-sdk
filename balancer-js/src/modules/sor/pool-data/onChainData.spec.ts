2ea57f50fca473884d74a8831ac8531c0c1eab23
Bruno Eidam Guerios <bruno@balancerlabs.dev>
2023-07-26 14:24:31 -0300

    Add FX custom swap fees to multicall onChainData fetch

--- balancer-js/src/modules/sor/pool-data/onChainData.spec.ts
+++ balancer-js/src/modules/sor/pool-data/onChainData.spec.ts
@@ -2,22 +2,36 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { expect } from 'chai';
 import { cloneDeep } from 'lodash';
-import { BALANCER_NETWORK_CONFIG, Network, PoolsSubgraphRepository } from '@/.';
-import { getOnChainPools } from './onChainData';
 import { JsonRpcProvider } from '@ethersproject/providers';
+
+import {
+  BALANCER_NETWORK_CONFIG,
+  Network,
+  Pool,
+  PoolsSubgraphRepository,
+} from '@/.';
+
+import { getOnChainBalances, getOnChainPools } from './onChainData';
 
 // yarn test:only ./src/modules/sor/pool-data/onChainData.spec.ts
 describe('getOnChainPools', async function () {
-  it('should fetch onchain pools', async function () {
-    const network = Network.POLYGON;
-    const rpcUrl = `https://polygon-mainnet.infura.io/v3/${process.env.INFURA}`;
-    const provider = new JsonRpcProvider(rpcUrl);
-    const url = BALANCER_NETWORK_CONFIG[network].urls.subgraph;
+  this.timeout(40000);
+  let pools: Pool[];
+  let provider: JsonRpcProvider;
+  const network = Network.POLYGON;
+  const url = BALANCER_NETWORK_CONFIG[network].urls.subgraph;
+  const rpcUrl = `https://polygon-mainnet.infura.io/v3/${process.env.INFURA}`;
+
+  before(async function () {
+    provider = new JsonRpcProvider(rpcUrl);
     const poolsRepo = new PoolsSubgraphRepository({
       url,
       chainId: network,
     });
-    const pools = await poolsRepo.all();
+    pools = await poolsRepo.all();
+  });
+
+  it('should fetch onchain pools using queries helper contract', async function () {
     const onChainPools = await getOnChainPools(
       cloneDeep(pools),
       BALANCER_NETWORK_CONFIG[network].addresses.contracts.poolDataQueries,
@@ -25,5 +39,15 @@ describe('getOnChainPools', async function () {
       provider
     );
     expect(onChainPools.length).to.be.gt(0);
-  }).timeout(40000);
+  });
+
+  it('should fetch onchain pools using multicall', async function () {
+    const onChainBalances = await getOnChainBalances(
+      cloneDeep(pools),
+      BALANCER_NETWORK_CONFIG[network].addresses.contracts.multicall,
+      BALANCER_NETWORK_CONFIG[network].addresses.contracts.vault,
+      provider
+    );
+    expect(onChainBalances.length).to.be.gt(0);
+  });
 });
