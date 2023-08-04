3d9d65c87bd56a5fd16c0f085274ac904fa6df45
Fábio Mendes <ms.fabiomendes@gmail.com>
2023-08-04 19:06:36 -0300

    fix zkevm gauges inflation rate

--- balancer-js/src/modules/data/liquidity-gauges/provider.ts
+++ balancer-js/src/modules/data/liquidity-gauges/provider.ts
@@ -71,6 +71,7 @@ export class LiquidityGaugeSubgraphRPCProvider
         '0x22625eedd92c81a219a83e1dc48f88d54786b017', // Polygon
         '0x6817149cb753bf529565b4d023d7507ed2ff4bc0', // Arbitrum
         '0x83e443ef4f9963c77bd860f94500075556668cb8', // Gnosis
+        '0x2498a2b0d6462d2260eac50ae1c3e03f4829ba95', // zkEVM
       ];
 
       const childGaugeAddresses = gauges
