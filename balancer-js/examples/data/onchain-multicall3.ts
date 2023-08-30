af598c8f8744355d343de1a36941bcb2c85355c0
bronco <rg3mvdb0@protonmail.com>
2023-08-30 13:08:00 +0200

    onchain data via multicall3 fixes

--- balancer-js/examples/data/onchain-multicall3.ts
+++ balancer-js/examples/data/onchain-multicall3.ts
@@ -1,35 +1,20 @@
-import { PoolsSubgraphRepository } from '@/modules/data/pool/subgraph'
-import { getOnChainBalances as getOnChainBalances3 } from '@/modules/sor/pool-data/onChainData3'
-import { SubgraphPoolBase, Network } from '@/.'
-import { getOnChainBalances } from '@/modules/sor/pool-data/onChainData'
-import { JsonRpcProvider } from '@ethersproject/providers'
-import _ from 'lodash'
+import {
+  PoolsSubgraphRepository,
+  SubgraphPoolBase,
+  Network,
+  BALANCER_NETWORK_CONFIG,
+  getOnChainBalances as getOnChainBalances3,
+} from '@balancer-labs/sdk';
+import { JsonRpcProvider } from '@ethersproject/providers';
+// import _ from 'lodash';
+
+// Importing legacy multicall fetcher for comparison
+// import { getOnChainBalances } from '@/modules/sor/pool-data/onChainData';
 
 const network = Network.POLYGON;
 
-const urls: Record<number, string> = {
-  [Network.MAINNET]:
-    'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2',
-  [Network.POLYGON]:
-    'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-polygon-v2',
-  [Network.ARBITRUM]:
-    'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-arbitrum-v2',
-  [Network.OPTIMISM]:
-    'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-optimism-v2',
-  [Network.GNOSIS]:
-    'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-gnosis-chain-v2',
-  [Network.AVALANCHE]:
-    'https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-avalanche-v2',
-  [Network.BASE]:
-    'https://api.studio.thegraph.com/query/24660/balancer-base-v2/version/latest',
-  [Network.FANTOM]:
-    'https://api.thegraph.com/subgraphs/name/beethovenxfi/beethovenx-v2-fantom',
-  [Network.ZKEVM]:
-    'https://api.studio.thegraph.com/query/24660/balancer-polygon-zk-v2/version/latest',
-};
-
 const pools = new PoolsSubgraphRepository({
-  url: urls[network],
+  url: BALANCER_NETWORK_CONFIG[network].urls.subgraph,
   chainId: network,
   query: {
     args: {
@@ -39,7 +24,7 @@ const pools = new PoolsSubgraphRepository({
       orderDirection: 'desc',
       where: {
         poolType: {
-          eq: "GyroE"
+          eq: 'GyroE',
         },
       },
     },
@@ -59,32 +44,44 @@ const providers: Record<number, JsonRpcProvider> = {
 
 const provider = providers[network];
 
-function findNestedValueDifferences(object1: any, object2: any, path = ''): any {
-  const allKeys = _.union(Object.keys(object1), Object.keys(object2))
+/**
+ * Recursively finds differences between two objects. Use to compare onchain data from 2 different fetchers.
+ */
+// function diffObjects(
+//   object1: any,
+//   object2: any,
+//   path = ''
+// ): any {
+//   const allKeys = _.union(Object.keys(object1), Object.keys(object2));
 
-  const differences = []
+//   const differences = [];
 
-  for (const key of allKeys) {
-    const newPath = path ? `${path}.${key}` : key
+//   for (const key of allKeys) {
+//     const newPath = path ? `${path}.${key}` : key;
 
-    if (_.isObject(object1[key]) && _.isObject(object2[key])) {
-      differences.push(...findNestedValueDifferences(object1[key], object2[key], newPath))
-    } else if (!_.isEqual(object1[key], object2[key])) {
-      differences.push({
-        path: newPath,
-        value1: object1[key],
-        value2: object2[key]
-      })
-    }
-  }
+//     if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+//       differences.push(...diffObjects(object1[key], object2[key], newPath));
+//     } else if (!_.isEqual(object1[key], object2[key])) {
+//       differences.push({
+//         path: newPath,
+//         value1: object1[key],
+//         value2: object2[key],
+//       });
+//     }
+//   }
 
-  return differences
-}
+//   return differences;
+// }
 
 async function main() {
-  const subgraph = await pools.fetch() as SubgraphPoolBase[];
-  const onchain3 = await getOnChainBalances3(subgraph, '', '0xBA12222222228d8Ba445958a75a0704d566BF2C8', provider);
-  console.log(onchain3.length)
+  const subgraph = (await pools.fetch()) as SubgraphPoolBase[];
+  const onchain3 = await getOnChainBalances3(
+    subgraph,
+    '',
+    '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
+    provider
+  );
+  console.log(onchain3.length);
   console.log(JSON.stringify(onchain3, null, 2));
   // const onchain = await getOnChainBalances(subgraph, '0xeefba1e63905ef1d7acba5a8513c70307c1ce441', '0xBA12222222228d8Ba445958a75a0704d566BF2C8', provider);
   // console.log(onchain.length)
@@ -100,6 +97,6 @@ async function main() {
   // }
 }
 
-main()
+main();
 
-// yarn example ./examples/data/onchain-multicall3.ts
+// yarn example ./debug/onchain-multicall3.ts
