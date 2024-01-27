5d8aed380e1fc7365f9eb0171ae570f4d9f314b5
Luiz Gustavo Abou Hatem De Liz <luizgustavo@balancerlabs.dev>
2024-01-27 18:56:03 -0300

    fixing lint errors;

--- balancer-js/src/lib/utils/coingecko-api.ts
+++ balancer-js/src/lib/utils/coingecko-api.ts
@@ -1,11 +1,11 @@
-export function getCoingeckoApiBaseUrl(isDemoApi = true) {
+export function getCoingeckoApiBaseUrl(isDemoApi = true): string {
   if (isDemoApi) {
     return 'https://api.coingecko.com/api/v3/';
   }
   return 'https://pro-api.coingecko.com/api/v3/';
 }
 
-export function getCoingeckoApiKeyHeaderName(isDemoApi = true) {
+export function getCoingeckoApiKeyHeaderName(isDemoApi = true): string {
   if (isDemoApi) {
     return 'x-cg-demo-api-key';
   }
